5b54cfb043d6b41a1c616bf7e1235ccdc9c94cbc
weishakeji <10522779@qq.com>
2018-03-17 11:14:44 +0800

    版权信息在html中的json处理

---
+++ Song.Site/Ajax/Copyright.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="Copyright.ashx.cs" Class="Song.Site.Ajax.Copyright" %>
---
+++ Song.Site/Ajax/Copyright.ashx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WeiSha.Common;
+using Song.ServiceInterfaces;
+
+namespace Song.Site.Ajax
+{
+    /// <summary>
+    /// 系统的版权信息，来自copyright.xml
+    /// </summary>
+    public class Copyright : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            //版权信息
+            WeiSha.Common.Copyright<string, string> copyright = WeiSha.Common.Request.Copyright;
+            string json = copyright.ToJson();
+            context.Response.Write(json);
+            context.Response.End();
+        }
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
