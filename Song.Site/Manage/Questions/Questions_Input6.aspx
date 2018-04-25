06c0d63fd40a4f17995d16ea1d3069073a017c56
weishakeji <10522779@qq.com>
2018-04-25 10:39:40 +0800

    增加公共题干

---
+++ Song.Site/Manage/Questions/Questions_Input6.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Manage/PageWin.Master" AutoEventWireup="true"
+    Codebehind="Questions_Input6.aspx.cs" Inherits="Song.Site.Manage.Questions.Questions_Input6"
+    Title="无标题页" %>
+
+<%@ MasterType VirtualPath="~/Manage/PageWin.Master" %>
+<%@ Register Assembly="WeiSha.WebControl" Namespace="WeiSha.WebControl" TagPrefix="cc1" %>
+<%@ Register Src="~/Manage/Utility/ExcelInput.ascx" TagName="ExcelInput" TagPrefix="uc1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cphMain" runat="server">
+   公共题干，暂不支持导入
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphBtn" runat="server">
+    <cc1:CloseButton ID="CloseButton1" runat="server" />
+</asp:Content>
---
+++ Song.Site/Manage/Questions/Questions_Input6.aspx.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.OleDb;
+
+using WeiSha.Common;
+
+using Song.ServiceInterfaces;
+using Song.Entities;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Song.Site.Manage.Questions
+{
+    public partial class Questions_Input6 : Extend.CustomPage
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+       
+      
+    }
+}
