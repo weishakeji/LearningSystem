7de06a47d5e365e6727b41d62fc45eb86d6c5787
weishakeji <10522779@qq.com>
2018-11-29 16:04:52 +0800

    修正课程购买css没有加载的问题

--- Song.Site/Templates/Web/_Public/Module/Course/buy.js
+++ Song.Site/Templates/Web/_Public/Module/Course/buy.js
@@ -1,130 +1,150 @@
 ﻿$(function () {
-    selectedInit();
-    subjectEvent();
-	//搜索以及分页导航
-	$("input[name=search]").val(decodeURI($().getPara("search")));
-	var paras=$().getPara();
-	if(paras!=null && paras.length>0){
-		$("#pagerBar a").each(function(index, element) {
-			var href=$(this).attr("href");
-            for(var i=0;i<paras.length;i++){
-				href=$().setPara(href,paras[i].key,paras[i].value);
-			}
-			$(this).attr("href",href);
-        });
-	}
+    _btnEvent();
+    _selectPrice();
+    //计算日均价格
+    $(".alt").each(function (index, element) {
+        var span = Number($(this).attr("span"));
+        var unit = $.trim($(this).attr("unit"));
+        var price = Number($(this).attr("price"));
+        var day = span;
+        if (unit == "日") day = span;
+        if (unit == "周") day = span * 7;
+        if (unit == "月") day = span * 30;
+        if (unit == "年") day = span * 365;
+        var result = Math.floor(price / day * 100) / 100;
+        $(this).find("b").text(result);
+    });
 });
-
-
-//各种选择按钮的事件
-function subjectEvent() {
-    $(".selectItem a").click(function () {
-        var attr = $(this).attr("attr");
-        var val = $(this).attr(attr);
-        //要清除的参数
-        var clear = $(this).attr("clear");
-        var url = buildUrl(attr, val, clear);
-        window.document.location.href = url;
+//按钮事件
+function _btnEvent() {
+    //免费课程的学习按钮
+    $("#btnStudy").click(function () {
+        //BuySubmit(0, 0);
+        //return false;
+    });
+    //确定按钮
+    $("#btnBuyStudy").click(function () {
+        var moneySpan = $("#money"); //学员资金余额
+        var couponSpan = $("#coupon"); //学员卡券余额
+        if (moneySpan.size() < 1) {
+            var txt = "您还没有登录，请登录后购买。";
+            var msg = new MsgBox("未登录", txt, 400, 300, "confirm");
+            msg.EnterEvent = function () {
+                window.location.href = "Login.ashx";
+            };
+            msg.Open();
+            return false
+        }
+        //是否选项费用项
+        var selected = $(".priceSelected");
+        if (selected.size() < 1) {
+            new MsgBox("提示", "请选择学习费用的选项。", 400, 300, "alert").Open();
+            return false;
+        }
+        //判断产品价格不得低于余额
+        var money = Number(moneySpan.html()); //余额
+        var coupon = Number(couponSpan.html()); //卡券余额
+        var mprice = Number($(this).attr("mprice"));    //价格，资金
+        var cprice = Number($(this).attr("cprice"));    //价格，卡券
+        //满足条件的判断
+        var tm = money >= mprice || (money >= (mprice - cprice) && (coupon >= cprice));
+        //资金余额不足
+        if (!tm) {
+            var msg = new MsgBox("提示", "你的余额不足，是否充值？", 400, 300, "confirm");
+            msg.EnterEvent = function () {
+                $(".btnRecharge").get(0).click();
+            };
+            msg.Open();
+            return false
+        }
+        if (!Verify.IsPass($("form.money-area"))) return false;
+        BuySubmit(1, 0);
         return false;
     });
 }
-//初始化
-function selectedInit() {
-    var strs = GetRequest();
-    if (strs.length < 1) {
-        $("#SelectedBar").hide();
-    } else {
-        //参数数组
-        var strs = GetRequest();
-        $("#Selected").html("");
-        for (var i = 0; i < strs.length; i++) {
-			if(strs[i][0]=="search"){
-				 var tm = "<div class=\"selectedItem\" clear=\"" + clear + "\">";
-				tm += "<div class='name'>查询-" +  decodeURIComponent(strs[i][1]) + "</div><div class='close' attr='" + strs[i][0] + "'>&nbsp;</div>";
-				tm += "</div> ";
-				$("#Selected").append(tm);
-				break;
-			}
-            var item = $(".selectItem a[" + strs[i][0] + "=" + strs[i][1] + "]");
-            if (item.size() < 1) continue;
-            //主题，如专业、类型的文字信息
-            var theme = item.parent().parent().prev().text();
-            var txt = item.text();
-            var clear = item.attr("clear");
-            var tm = "<div class=\"selectedItem\" clear=\"" + clear + "\">";
-            tm += "<div class='name'>" + theme + txt + "</div><div class='close' attr='" + strs[i][0] + "'>&nbsp;</div>";
-            tm += "</div> ";
-            $("#Selected").append(tm);
+//选择价格
+function _selectPrice() {
+    //价格选项的点击事件
+    $(".priceItem").click(function () {
+        $(this).parent().find(".priceItem").removeClass("priceSelected");
+        $(this).parent().find(".priceItem span.ico").html("&#xf00c6;");
+        //选中事件
+        $(this).addClass("priceSelected");
+        $(this).find("span.ico").html("&#xe667;");
+        //如果登录，则显示验证码
+        var moneySpan = $("#money");
+        if (moneySpan.size() > 0) {
+            $(".verifyInfo").show();
         }
-        //选中的专业在鼠标经过时
-        $("#Selected .selectedItem").hover(function () {
-            $(this).addClass("selectedItemOver");
-            $(this).find(".close").addClass("closeOver");
-        }, function () {
-            $(this).removeClass("selectedItemOver");
-            $(this).find(".close").removeClass("closeOver");
-        });
-        $("#Selected .close").click(function () {
-            var clear = $(this).parent().attr("clear");
-            var url = buildUrl($(this).attr("attr"), null, clear);
-            window.document.location.href = url;
-        });
-    }
-	//如果没有任何选择项，则不显示已选择的区域
-	if($("#Selected div").size()<1){
-		$("#SelectedBar").hide();
-	}
+        //设置价格在购买按钮上，取值时方便
+        var selected = $(".priceSelected");
+        var mprice = Number(selected.find(".mprice").html()); //选中项的资金价格
+        var cprice = Number(selected.find(".cprice").html()); //选中项的卡券价格
+        $("#btnBuyStudy").attr("mprice", mprice).attr("cprice", (isNaN(cprice) ? 0 : cprice));
+    });
+    $(".priceItem:eq(0)").click();
 }
-//生成地址
-function buildUrl(attr, val, clear) {
-    //当前页面的上级路径，因为子页面没有写路径，默认与本页面同路径
-    var url = String(window.document.location.href);
-    url = url.replace(/#/ig, "");
-    if (url.indexOf("?") > -1) url = url.substring(0, url.lastIndexOf("?"));
-    //参数数组
-    var strs = GetRequest();
-    var para = new Array();
-    var isHave = false;
-    for (var i = 0; i < strs.length; i++) {
-        if (strs[i][0] == attr) {
-            strs[i][1] = val;
-            isHave = true;
-        }
-        if (!(strs[i][0] == attr && val == null)) {
-            para.push(strs[i]);
-        }
-    }
-    if (!isHave && val != null) para.push(new Array(attr, val));
-    if (para.length < 1) return url;
-    url += "?";
-    //要清除的数据
-    var clr = clear == null ? new Array() : clear.split(",");
-    for (var i = 0; i < para.length; i++) {
-        var tmIsHave = false;
-        for (var j = 0; j < clr.length; j++) {
-            if (para[i][0] == clr[j]) {
-                tmIsHave = true;
-                break;
+
+//提交购买操作
+//isfree:是否免费购买，免费请写0
+//istry:是否试用，不试用请写0
+function BuySubmit(isfree, istry) {
+    var urlPath = "/Ajax/CourseBuySubmit.ashx?timestamp=" + new Date().getTime();
+    var code = $.trim($(".verify").val());         //验证码
+    var cpid = $(".priceSelected").attr("cpid");  //价格项的id
+    var couid = $().getPara("couid");    //课程id
+    var return_url = "CourseStudy.ashx";            //成功后，跳转的页面
+    $.ajax({
+        type: "POST", url: urlPath, dataType: "text",
+        data: { veriCode: code, cpid: cpid, couid: couid, return_url: return_url,
+            isfree: isfree, istry: istry
+        },
+        //开始，进行预载
+        beforeSend: function (XMLHttpRequest, textStatus) {
+            window.isSubmit = true;
+            var msg = new MsgBox("提交", "正在处理交易，请稍等……", 400, 300, "loading");
+            msg.ShowCloseBtn = false;
+            msg.Open();
+        },
+        //加载出错
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            var msg = new MsgBox("服务器故障", "无法获取响应<br/><br/>错误信息：" + errorThrown, 400, 300, "alert");
+            msg.Open();
+        },
+        //加载成功！
+        success: function (data) {
+            try {
+                var result = eval("(" + data + ")");
+                if (result.status == 0) {
+                    var msg = new MsgBox("操作成功", "操作成功，点击确定开始学习", 400, 300, "confirm");
+                    msg.EnterEvent = function () {
+                        window.location.href = result.return_url + "?couid=" + result.couid;
+                    };
+                    msg.Open();
+                }
+                var error = "";
+                if (result.status == 1) error = "您还未登录！";
+                if (result.status == 2) error = "验证码不正确！";
+                if (result.status == 3) error = "价格数据不存在，请刷新界面再提交！";
+                if (result.status == 4) error = "当前课程不存在，请刷新界面再提交！";
+                if (result.status == 5) error = "余额不足，请充值！";
+                if (result.status == 6) error = "数据异常！";
+                if (result.status == 7) error = "当前课程并不是免费的！";
+                if (result.status == 7) error = "当前课程不允许试用！";
+                if (result.status != 0) {
+                    var errinfo = "原因：" + error;
+                    errinfo += "<br/>说明：" + result.errinfo;
+                    errinfo += "<br/>详情：<a href=\'" + result.logfile + "' target='_blank'>点击查看</a>";
+                    var msg = new MsgBox("发生错误", errinfo, 400, 300, "alert");
+                    msg.OverEvent = function () {
+                        MsgBox.Close();
+                    };
+                    msg.Open();
+                }
+                window.isSubmit = false;
+            } catch (e) {
+                alert(data);
             }
         }
-        if (tmIsHave) continue;
-        url += para[i][0] + "=" + para[i][1];
-        if (i < para.length - 1) url += "&";
-    }
-    if (url.substring(url.length - 1) == "?") url = url.substring(0, url.length - 1);
-    if (url.substring(url.length - 1) == "&") url = url.substring(0, url.length - 1);
-    return url;
-}
-//获取参数
-function GetRequest() {
-    var url = location.search; //获取url中"?"符后的字串
-    var theRequest = new Array();
-    if (url.indexOf("?") != -1) {
-        var str = url.substr(1);
-        strs = str.split("&");
-        for (var i = 0; i < strs.length; i++) {
-            theRequest.push(new Array(strs[i].split("=")[0], strs[i].split("=")[1]));
-        }
-    }
-    return theRequest;
+    });
 }
