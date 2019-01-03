3a21cf33c42a505c5c9445f3072329e6dce17ca5
weishakeji <10522779@qq.com>
2019-01-03 14:42:24 +0800

    手机端手机首页增加侧滑菜单

---
+++ Song.Site/Ajax/Self.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="Self.ashx.cs" Class="Song.Site.Ajax.Self" %>
---
+++ Song.Site/Ajax/Self.ashx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WeiSha.Common;
+namespace Song.Site.Ajax
+{
+    /// <summary>
+    /// 获取当前登录账户信息
+    /// </summary>
+    public class Self : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            //当前登录账号
+            Song.Entities.Accounts acc = Extend.LoginState.Accounts.CurrentUser;
+            string tm = "{\"login\":{islogin}";
+            if (acc == null) tm = tm.Replace("{islogin}", false.ToString().ToLower());
+            if (acc != null)
+            {
+                tm = tm.Replace("{islogin}", true.ToString().ToLower());
+                //资源的路径
+                //string resPath = Upload.Get["Accounts"].Virtual;
+                //acc.Ac_Photo = resPath + acc.Ac_Photo;
+                tm += ",\"object\":" + acc.ToJson(null, "Ac_Pw,Ac_Qus,Ac_Ans,Ac_IDCardNumber");
+            }
+            tm += "}";
+            context.Response.Write(tm);
+            context.Response.End();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
