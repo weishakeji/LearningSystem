75a023517af71ee08dac01965a42b9c67991f4bf
weishakeji <10522779@qq.com>
2019-01-17 12:15:55 +0800

    增加手机端试题练习导引

--- Song.Site/Templates/Mobi/Default2019/Scripts/QuesExercisesGuide.js
+++ Song.Site/Templates/Mobi/Default2019/Scripts/QuesExercisesGuide.js
@@ -8,11 +8,11 @@ var guide = {
     },
     html: function () {
         var html = "<div id='guide-box'>";
-		html+="<div class='slide-top-box'><i>&#xe6d8;</i>设置视图模式</div>";
-		html+="<div class='slide-chest-box'><i>&#xe63d;</i>滑动切换试题<i>&#xe6aa;</i></div>";
-		html+="<div class='slide-waist-box'><row><i>&#xe631;</i>缩小字体</row><row>放大字体<i>&#xe636</i></row></div>";
-		html+="<div class='slide-foot-box'><span>答题卡</span><i>&#xe786;</i></div>";
-		html+="</div>";
+        //html+="<div class='slide-top-box'><i>&#xe6d8;</i>设置视图模式</div>";
+        html += "<div><i>&#xe63d;</i>滑动切换试题<i>&#xe6aa;</i></div>";
+        html += "<div><i>&#xe631;</i>捏合缩放字体<i>&#xe636</i></div>";
+        //html+="<div class='slide-foot-box'><span>答题卡</span><i>&#xe786;</i></div>";
+        html += "</div>";
         return html;
     },
     builder: function () {
@@ -21,21 +21,23 @@ var guide = {
         var hg = $(window).height() > document.body.clientHeight ? $(window).height() : document.body.clientHeight;
         var wd = $(window).width() > document.body.clientWidth ? $(window).width() : document.body.clientWidth;
         box.width(wd).height(hg);
-        box.css({ "position": "absolute", "z-index": "100" });
+        box.css({ "position": "absolute", "z-index": "100", "background-color": "rgba(0,0,0,0.6)", "color": "#fff" });
         box.css({ left: 0, top: 0 });
-        var alpha = 50;
-        //box.css("background-color", "#000");
-        //box.css("filter", "Alpha(Opacity=" + alpha + ")").css("display", "block");
-        //box.css("-moz-opacity", alpha / 100).css("opacity", alpha / 100);
         box.show();
-		this.events(box);
+        //样式
+        box.find(">div").each(function (index, element) {
+            $(this).css({ "position": "absolute", "width": "100%", "text-align": "center", "font-size": "20px", "top": (index + 1) * 160 });
+            $(this).find("i").css({ "font-family": "iconfont", "font-size": "60px", "margin": "0px 30px 0px 20px", "text-shadow": "0px 0px 20px #fff" });
+        });
+        box.find(">div:last").css({ "font-size": "15px" }).find("i").css({ "font-size": "40px" });
+        this.events(box);
     },
     events: function (box) {
-		box.click(function(){			
-			$(this).fadeOut(500);
-		});
-		window.setTimeout(function(){
-			//$("#guide-box").fadeOut(500);
-		},5000);
+        box.click(function () {
+            $(this).fadeOut(500);
+        });
+        window.setTimeout(function () {
+            //$("#guide-box").fadeOut(500);
+        }, 5000);
     }
 };
