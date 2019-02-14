424a44c7d38cda9abb037232285e41aaaa4d55ca
weishakeji <10522779@qq.com>
2019-02-14 14:34:00 +0800

    修正手机端教师介绍页无法上下滚动问题

--- Song.Site/Templates/Mobi/Default2019/Scripts/Teachers.js
+++ Song.Site/Templates/Mobi/Default2019/Scripts/Teachers.js
@@ -12,6 +12,9 @@
 		var thid=this.getAttribute("thid");		//教师id
 		new PageBox(name, "Teacher.ashx?id="+thid, 100, 100, "url").Open();
 	});
+	mui('.mui-scroll-wrapper').scroll({
+		deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+	});
 });
 
 //设置星标
