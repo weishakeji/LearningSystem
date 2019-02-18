980a1f406ee373598d1a1569a4f169614e399d0f
weishakeji <10522779@qq.com>
2019-02-18 19:06:27 +0800

    手机端首页图片设置最小高度

---
+++ Song.Site/Debug.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Song.Site
+{
+    public partial class Debug : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = Request.ServerVariables["HTTP_USER_AGENT"]; ;
+        }
+    }
+}
