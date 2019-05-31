b1a7026c92bda58c73f756a9393420b746af9830
weishakeji <10522779@qq.com>
2019-05-31 09:49:37 +0800

    知识库需要购买课程后（或课程免费）才能阅读

--- Song.Site/Knowledge.ashx.cs
+++ Song.Site/Knowledge.ashx.cs
@@ -18,16 +18,26 @@ namespace Song.Site
         {
             
             //知识       
-            Song.Entities.Knowledge knl = Business.Do<IKnowledge>().KnowledgeSingle(id);
+            Song.Entities.Knowledge knl = Business.Do<IKnowledge>().KnowledgeSingle(id);            
             this.Document.Variables.SetValue("knl", knl);
             if (knl != null)
             {
-                Song.Entities.Course cou = Business.Do<ICourse>().CourseSingle(knl.Cou_ID);
-                this.Document.Variables.SetValue("course", cou);
-                //上级专业
-                if (cou != null)
+                Song.Entities.Course course = Business.Do<ICourse>().CourseSingle(knl.Cou_ID);
+                //是否免费，或是限时免费
+                if (course.Cou_IsLimitFree)
                 {
-                    List<Song.Entities.Subject> sbjs = Business.Do<ISubject>().Parents(cou.Sbj_ID, true);
+                    DateTime freeEnd = course.Cou_FreeEnd.AddDays(1).Date;
+                    if (!(course.Cou_FreeStart <= DateTime.Now && freeEnd >= DateTime.Now))
+                        course.Cou_IsLimitFree = false;
+                }
+                this.Document.Variables.SetValue("course", course);
+                //是否购买课程（免费的也可以学习）
+                bool isBuy = course.Cou_IsFree || course.Cou_IsLimitFree ? true : Business.Do<ICourse>().IsBuy(course.Cou_ID, this.Account.Ac_ID);
+
+                //上级专业
+                if (course != null)
+                {
+                    List<Song.Entities.Subject> sbjs = Business.Do<ISubject>().Parents(course.Sbj_ID, true);
                     this.Document.Variables.SetValue("sbjs", sbjs);
                 }
             }
