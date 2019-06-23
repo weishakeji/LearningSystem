a55414e3c5b3d86145878a797675eba457d10931
weishakeji <10522779@qq.com>
2019-06-23 17:04:19 +0800

    编写通过学习记录查询学员账号的界面

--- Song.ViewData/Methods/Account.cs
+++ Song.ViewData/Methods/Account.cs
@@ -52,6 +52,55 @@ namespace Song.ViewData.Methods
             return accs;
         }
         /// <summary>
+        /// 按账号和姓名查询学员
+        /// </summary>
+        /// <param name="acc"></param>
+        /// <param name="name">姓名，可模糊查询</param>
+        /// <returns></returns>
+        public Song.Entities.Accounts[] Seach(string acc, string name)
+        {
+            List<Song.Entities.Accounts> list = new List<Accounts>();
+            Song.Entities.Accounts[] accs = Business.Do<IAccounts>().Account4Name(name);
+            foreach (Song.Entities.Accounts ac in accs)
+                list.Add(ac);
+            Song.Entities.Accounts account = Business.Do<IAccounts>().AccountsSingle(acc, -1);
+            if (account != null)
+            {
+                bool isExist = false;
+                foreach (Song.Entities.Accounts ac in accs)
+                {
+                    if (ac.Ac_ID == account.Ac_ID)
+                    {
+                        isExist = true;
+                        break;
+                    }
+                }
+                if (!isExist) list.Add(account);
+            }
+            foreach (Song.Entities.Accounts ac in list)          
+                ac.Ac_Pw = string.Empty;
+            return list.ToArray<Accounts>();
+        }
+        /// <summary>
+        /// 从学习记录中获取学员记录
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Song.Entities.LogForStudentStudy[] ForLogs(string acc, string name)
+        {
+            string sql = @"select Ac_ID,Ac_AccName, Ac_Name from (
+select logs.* from Accounts right join 
+(select * from LogForStudentStudy where 
+{name} and {acc}) as logs
+on Accounts.Ac_ID=Logs.Ac_ID) as tm
+ group by Ac_ID,Ac_AccName,Ac_Name";
+            sql = sql.Replace("{name}", string.IsNullOrWhiteSpace(name) ? "1=1" : "Ac_Name like '%" + name + "%'");
+            sql = sql.Replace("{acc}", string.IsNullOrWhiteSpace(acc) ? "1=1" : "Ac_AccName='" + acc + "'");
+
+            Song.Entities.LogForStudentStudy[] accs = Business.Do<ISystemPara>().ForSql<LogForStudentStudy>(sql).ToArray<LogForStudentStudy>();
+            return accs;
+        }
+        /// <summary>
         /// 学员的视频学习记录
         /// </summary>
         /// <param name="acid">学员id</param>
