54567e35617bc16c8f053799c6b61b24958ad360
weishakeji <10522779@qq.com>
2019-07-15 10:05:07 +0800

    修正一些样式细节

--- Song.Site/Manage/Student/Scripts/Students_Details.js
+++ Song.Site/Manage/Student/Scripts/Students_Details.js
@@ -18,12 +18,12 @@
     });
     //打印按钮事件
     $("input#btnPrint").click(function () {
-        //$("body .page").jqprint();
-        $("body .page").jqprint({
+        $("body .page").jqprint();
+        /*$("body .page").jqprint({
             debug: false, //如果是true则可以显示iframe查看效果（iframe默认高和宽都很小，可以再源码中调大），默认是false
-            importCSS: true, //true表示引进原来的页面的css，默认是true。（如果是true，先会找$("link[media=print]")，若没有会去找$("link")中的css文件）
+            //importCSS: true, //true表示引进原来的页面的css，默认是true。（如果是true，先会找$("link[media=print]")，若没有会去找$("link")中的css文件）
             printContainer: true, //表示如果原来选择的对象必须被纳入打印（注意：设置为false可能会打破你的CSS规则）。
             operaSupport: true//表示如果插件也必须支持歌opera浏览器，在这种情况下，它提供了建立一个临时的打印选项卡。默认是true
-        });
+        });*/
     });
 });
