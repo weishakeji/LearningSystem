238b5be78954d6f45c5bb32dfe1225de0d11cb2b
weishakeji <10522779@qq.com>
2019-08-23 10:11:26 +0800

    更新Ckplayer视频播放器

--- Song.Site/Templates/Web/_Public/Scripts/CourseStudy.js
+++ Song.Site/Templates/Web/_Public/Scripts/CourseStudy.js
@@ -22,6 +22,8 @@
             if (vdata.state.isAccess) vdata.titState = 'isAccess';
             if (vdata.state.isQues) vdata.titState = 'isQues';
             if (vdata.state.existVideo) vdata.titState = 'existVideo';
+            //视频播放
+            vdata.videoPlay(vdata.state);
         }
     },
     methods: {
@@ -67,6 +69,22 @@
                     if (!state.data.success) alert("章节状态加载错误");
                 }
             }));
+        },
+        //视频播放
+        videoPlay:function(state){
+            var videoObject = {
+                container: '.videosamplex',//“#”代表容器的ID，“.”或“”代表容器的class
+                variable: 'player',//该属性必需设置，值等于下面的new chplayer()的对象
+                mobileCkControls:true,//是否在移动端（包括ios）环境中显示控制栏
+                mobileAutoFull:false,//在移动端播放后是否按系统设置的全屏播放
+                h5container:'#videoplayer',//h5环境中使用自定义容器
+                videoDbClick:false,     //禁止双击全屏
+                autoplay:true,          //自动播放
+                contextMenu: [['微厦科技', 'link', 'http://www.ckplayer.com', '_blank'], ['version:X1', 'default', 'line']],
+                video:state.urlVideo//视频地址
+              };
+              var player=new ckplayer(videoObject);
+              player.contextMenu= [['微厦科技', 'link', 'http://www.ckplayer.com', '_blank'], ['version:X1', 'default', 'line']];;
         }
     },
     created: function () {
