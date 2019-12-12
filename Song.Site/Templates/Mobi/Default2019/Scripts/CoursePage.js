a60df4b9491ac44df854ce405b33dd99d91c8cf1
weishakeji <10522779@qq.com>
2019-12-12 08:16:43 +0800

    优化：重构手机端课程详情页

--- Song.Site/Templates/Mobi/Default2019/Scripts/CoursePage.js
+++ Song.Site/Templates/Mobi/Default2019/Scripts/CoursePage.js
@@ -4,7 +4,9 @@ $(function () {
 //菜单项的事件
 function default_event() {
     //主菜单，即课程学习菜单的事件
-    mui('body').on('tap', '.mm-item', function () {
+    mui('body').on('tap', 'div.mm-item', function () {
+		var type=$(this).attr("type");
+		//if($.trim(type)=="open")return;
         var canStudy = $(this).parent().attr("canStudy");
 		var couid=$("body").attr("couid");
         if (canStudy != "True") {
