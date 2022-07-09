80ea08bb14102fac69ad2b062f5f0acd26d1d13e
weishakeji <songleiming@163.com>
2022-07-10 01:04:58 +0800

    编写第三方登录的管理界面

---
+++ Sourcecode/Song.ViewData/Methods/OtherLogin.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WeiSha.Core;
+using Song.Entities;
+using Song.ServiceInterfaces;
+using Song.ViewData.Attri;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using System.Web;
+using System.IO;
+
+namespace Song.ViewData.Methods
+{
+    /// <summary>
+    /// 第三方登录
+    /// </summary>
+    [HttpGet]
+    public class OtherLogin : ViewMethod, IViewAPI
+    {
+
+        #region QQ登录
+        /// <summary>
+        /// 获取QQ登录的配置信息
+        /// </summary>
+        /// <returns></returns>
+        public JObject QQ()
+        {
+            JObject jo = new JObject();
+            //qq登录
+            jo.Add("LoginIsUse", Business.Do<ISystemPara>()["QQLoginIsUse"].Boolean ?? true);
+            //是否允许qq直接注册登录
+            jo.Add("DirectIs", Business.Do<ISystemPara>()["QQDirectIs"].Boolean ?? true);
+            jo.Add("APPID", Business.Do<ISystemPara>()["QQAPPID"].String);
+            jo.Add("APPKey", Business.Do<ISystemPara>()["QQAPPKey"].String);
+            jo.Add("Returl", Business.Do<ISystemPara>()["QQReturl"].String);
+            return jo;
+        }
+        /// <summary>
+        /// 保存QQ登录的配置信息
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [SuperAdmin]
+        public bool QQUpdate(JObject data)
+        {
+            IEnumerable<JProperty> properties = data.Properties();
+            foreach (JProperty item in properties)
+            {
+                // item.Name 为 键
+                // item.Value 为 值
+                //if (item.Name == "QQLoginIsUse")
+                //    Business.Do<ISystemPara>().Save("QQLoginIsUse", item.Value.ToString(), false);
+                //if (item.Name == "QQDirectIs")
+                //    Business.Do<ISystemPara>().Save("QQDirectIs", item.Value.ToString(), false);
+
+                Business.Do<ISystemPara>().Save("QQ"+item.Name, item.Value.ToString(), false);             
+               
+            }
+            Business.Do<ISystemPara>().Refresh();
+            return true;
+        }
+        #endregion
+
+        #region 微信登录
+        /// <summary>
+        /// 获取Weixin登录的配置信息
+        /// </summary>
+        /// <returns></returns>
+        public JObject Weixin()
+        {
+            JObject jo = new JObject();
+            //微信登录
+            jo.Add("LoginIsUse", Business.Do<ISystemPara>()["WeixinLoginIsUse"].Boolean ?? true);
+            //是否允许微信直接注册登录
+            jo.Add("DirectIs", Business.Do<ISystemPara>()["WeixinDirectIs"].Boolean ?? true);
+
+            //微信开放平台设置
+            jo.Add("APPID", Business.Do<ISystemPara>()["WeixinAPPID"].String);
+            jo.Add("Secret", Business.Do<ISystemPara>()["WeixinSecret"].String);     
+            jo.Add("Returl", Business.Do<ISystemPara>()["WeixinReturl"].String);
+
+            //微信公众号设置
+            jo.Add("pubAPPID", Business.Do<ISystemPara>()["WeixinpubAPPID"].String);
+            jo.Add("pubSecret", Business.Do<ISystemPara>()["WeixinpubSecret"].String);
+            jo.Add("pubReturl", Business.Do<ISystemPara>()["WeixinpubReturl"].String);
+            return jo;
+        }
+        /// <summary>
+        /// 保存Weixin登录的配置信息
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [SuperAdmin]
+        public bool WeixinUpdate(JObject data)
+        {
+            IEnumerable<JProperty> properties = data.Properties();
+            foreach (JProperty item in properties)
+            {
+                Business.Do<ISystemPara>().Save("Weixin" + item.Name, item.Value.ToString(), false);
+            }
+            Business.Do<ISystemPara>().Refresh();
+            return true;
+        }
+        #endregion
+
+        #region 金碟云之家轻应用登录
+        /// <summary>
+        /// 获取金碟云之家登录的配置信息
+        /// </summary>
+        /// <returns></returns>
+        public JObject Yunzhijia()
+        {
+            JObject jo = new JObject();
+            //
+            jo.Add("LoginIsuse", Business.Do<ISystemPara>()["YunzhijiaLoginIsuse"].Boolean ?? true);
+            jo.Add("Appid", Business.Do<ISystemPara>()["YunzhijiaAppid"].String);
+            jo.Add("AppSecret", Business.Do<ISystemPara>()["YunzhijiaAppSecret"].String);
+            jo.Add("Domain", Business.Do<ISystemPara>()["YunzhijiaDomain"].String);
+            jo.Add("Acc", Business.Do<ISystemPara>()["YunzhijiaAcc"].String);        
+
+            return jo;
+        }
+        /// <summary>
+        /// 保存金碟云之家登录的配置信息
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [SuperAdmin]
+        public bool YunzhijiaUpdate(JObject data)
+        {
+            IEnumerable<JProperty> properties = data.Properties();
+            foreach (JProperty item in properties)
+            {
+                Business.Do<ISystemPara>().Save("Yunzhijia" + item.Name, item.Value.ToString(), false);
+            }
+            Business.Do<ISystemPara>().Refresh();
+            return true;
+        }
+        #endregion
+
+    }
+}
--- Sourcecode/Song.WebSite/Templates/Manage/Default/OtherLogin/Scripts/Setup.js
+++ Sourcecode/Song.WebSite/Templates/Manage/Default/OtherLogin/Scripts/Setup.js
@@ -1,5 +1,6 @@
 ﻿$ready(function () {
-    window.vm = new Vue({
+    window.vapp = new Vue({
+        el: '#vapp',
         data: {
             tabs: [
                 { name: 'QQ登录', tag: 'qq', icon: '&#xe82a', size: 16 },
@@ -7,63 +8,122 @@
                 { name: '金碟.云之家', tag: 'yunzhijia', icon: '&#xa021', size: 14 }
             ],
             activeName: 'qq',      //选项卡
-          
+
+            qq: {},
+            qq_rules: {
+                APPID: [
+                    { required: true, message: '不得为空', trigger: 'blur' }
+                ],
+                APPKey: [
+                    { required: true, message: '不得为空', trigger: 'blur' }
+                ],
+                Returl: [
+                    { required: true, message: '不得为空', trigger: 'blur' }
+                ]
+            },
+            weixin: {},
+            weixin_rules: {
+                APPID: [
+                    { required: true, message: '不得为空', trigger: 'blur' }
+                ],
+                Secret: [
+                    { required: true, message: '不得为空', trigger: 'blur' }
+                ],
+                Returl: [
+                    { required: true, message: '不得为空', trigger: 'blur' }
+                ],
+                pubAPPID: [
+                    { required: true, message: '不得为空', trigger: 'blur' }
+                ],
+                pubSecret: [
+                    { required: true, message: '不得为空', trigger: 'blur' }
+                ],
+                pubReturl: [
+                    { required: true, message: '不得为空', trigger: 'blur' }
+                ]
+            },
+            yunzhijia: {},
+            yunzhijia_rules: {
+                Appid: [
+                    { required: true, message: '不得为空', trigger: 'blur' }
+                ],
+                AppSecret: [
+                    { required: true, message: '不得为空', trigger: 'blur' }
+                ],
+                Domain: [
+                    { required: true, message: '不得为空', trigger: 'blur' }
+                ],
+                Acc: [
+                    { required: true, message: '不得为空', trigger: 'blur' }
+                ]
+            },
             loading: false,
-           
+
         },
         watch: {
         },
+        created: function () {
+            var th = this;
+            th.loading = true;
+            $api.bat(
+                $api.get('OtherLogin/QQ'),
+                $api.get('OtherLogin/Weixin'),
+                $api.get('OtherLogin/Yunzhijia')
+            ).then(axios.spread(function (qq, weixin, yunzhijia) {
+                th.loading = false;
+                //判断结果是否正常
+                for (var i = 0; i < arguments.length; i++) {
+                    if (arguments[i].status != 200)
+                        console.error(arguments[i]);
+                    var data = arguments[i].data;
+                    if (!data.success && data.exception != null) {
+                        console.error(data.exception);
+                        throw arguments[i].config.way + ' ' + data.message;
+                    }
+                }
+                //获取结果
+                th.qq = qq.data.result;
+                th.weixin = weixin.data.result;
+                th.yunzhijia = yunzhijia.data.result;
+            })).catch(function (err) {
+                th.loading = false;
+                Vue.prototype.$alert(err);
+                console.error(err);
+            });
+        },
         methods: {
             //保存信息
-            btnEnter: function () {
-                if (vm.loading) return;
-                this.$refs['form'].validate(function (valid) {
+            btnEnter: function (formName, entity) {
+                var th = this;
+                this.$refs[formName].validate((valid) => {
                     if (valid) {
-                        $api.post("live/Setup", vm.form).then(function (req) {
-                            var res = req.data;
-                            if (res.success) {
-                                vm.$message({ message: '操作成功', type: 'success' });
+                        th.loading = true;
+                        var apipath = "OtherLogin/" + formName + "Update"
+                        $api.post(apipath, { 'data': entity }).then(function (req) {
+                            th.loading = false;
+                            if (req.data.success) {
+                                var result = req.data.result;
+                                th.$message({
+                                    type: 'success',
+                                    message: '操作成功!',
+                                    center: true
+                                });
                             } else {
-                                throw res.data.message;
+                                throw req.data.message;
                             }
                         }).catch(function (err) {
-                            vm.$alert('错误信息：' + err, '操作失败', { confirmButtonText: '确定' });
+                            th.loading = false;
+                            Vue.prototype.$alert(err);
                         });
+                    } else {
+                        console.log('error submit!!');
+                        return false;
                     }
                 });
             },
-            //测试链接
-            btnTest: function () {
-                var ak = vm.form.AccessKey;
-                var sk = vm.form.SecretKey;
-                var hubname = vm.form.LiveSpace;
-                $api.get('Live/Test', { 'ak': ak, 'sk': sk, 'hubname': hubname }).then(function (req) {
-                    if (req.data.success) {
-                        var result = req.data.result;
-                        vm.$alert('测试通过', '成功',
-                            {
-                                dangerouslyUseHTMLString: true,
-                                confirmButtonText: '确定'
-                            });
-                    } else {
-                        throw req.data.message;
-                    }
-                }).catch(function (err) {
-                    var msg = "可能的原因：<br/>1、信息填写错误；<br/>2、七牛云账号欠费。";
-                    vm.$alert('错误信息：' + err + '<br/>' + msg, '操作失败',
-                        {
-                            dangerouslyUseHTMLString: true,
-                            confirmButtonText: '确定'
-                        });
-                });
-            }
-        },
-        created: function () {
-            $api.get("live/GetSetup").then(function (req) {
-                vm.form = req.data.result;
-            });
+
         }
     });
-    window.vm.$mount('#app-form');
+    //window.vapp.$mount('#vapp');
 
 });
