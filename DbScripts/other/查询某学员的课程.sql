0ea8149fb68f92aabb79d256fe468b66cb12cfbd
weishakeji <songleiming@163.com>
2022-10-10 12:25:18 +0800

    编写查询表和字段的脚本

--- DbScripts/other/查询某学员的课程.sql
+++ DbScripts/other/查询某学员的课程.sql
@@ -5,7 +5,7 @@ select ROW_NUMBER() OVER(Order by Cou_ID) AS 'rowid',*  from (
 select cou.* from Course as cou right join  Student_Course as sc
 on cou.Cou_ID = sc.Cou_ID
 where sc.Ac_ID=2 and sc.Stc_IsEnable=1
---and  sc.Stc_EndTime<getdate()
+and  sc.Stc_EndTime<getdate()  and sc.Stc_Type!=5
 --and (sc.Stc_StartTime<getdate() and  sc.Stc_EndTime>getdate())
 --order by sc.Stc_EndTime desc
 
---
+++ DbScripts/other/查询表和字段的信息.sql
@@ -0,0 +1,34 @@
+/*获取所有表*/
+select name from sysobjects where type='U'
+
+/* 表信息 */
+SELECT [TableName] = [Tables].name ,
+        [TableOwner] = [Schemas].name ,
+        [TableCreateDate] = [Tables].create_date ,
+        [TableModifyDate] = [Tables].modify_date
+FROM    sys.tables AS [Tables]
+        INNER JOIN sys.schemas AS [Schemas] ON [Tables].schema_id = [Schemas].schema_id
+WHERE   [Tables].name = 'Course'
+
+ /*查询表主键*/
+ SELECT distinct
+	TABLE_NAME,
+	COLUMN_NAME=stuff((
+		SELECT '|'+COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+		where TABLE_NAME ='Article'
+    FOR XML path('')
+    ), 1, 1, '')
+FROM
+	INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+where TABLE_NAME ='Article'
+
+
+/**表的字段信息，包括数据类型，长度*/
+SELECT name,type_name(xtype) AS type,
+--长度，无限长为-1
+length,(type_name(xtype)+'('+CONVERT(varchar,length)+')') as fulltype,
+--是否可空，0为可空
+isnullable
+FROM syscolumns
+WHERE (id = OBJECT_ID('course'))
+order by name asc
