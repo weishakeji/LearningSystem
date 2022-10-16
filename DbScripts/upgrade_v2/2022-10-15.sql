a90f8f1383feb35b23aa2940f77e24ead5a562b9
weishakeji <songleiming@163.com>
2022-10-16 18:44:39 +0800

    更新脚本

--- DbScripts/upgrade_v2/2022-10-15.sql
+++ DbScripts/upgrade_v2/2022-10-15.sql
@@ -11,3 +11,110 @@ GO
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Download]') AND type in (N'U'))
 DROP TABLE [dbo].[Download]
 GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DownloadOS]') AND type in (N'U'))
+DROP TABLE [dbo].[DownloadOS]
+GO
+
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DownloadType]') AND type in (N'U'))
+DROP TABLE [dbo].[DownloadType]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Forum]') AND type in (N'U'))
+DROP TABLE [dbo].[Forum]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[InnerEmail]') AND type in (N'U'))
+DROP TABLE [dbo].[InnerEmail]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Picture]') AND type in (N'U'))
+DROP TABLE [dbo].[Picture]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PictMiniature]') AND type in (N'U'))
+DROP TABLE [dbo].[PictMiniature]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Product]') AND type in (N'U'))
+DROP TABLE [dbo].[Product]
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Product_Package]') AND type in (N'U'))
+DROP TABLE [dbo].[Product_Package]
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ProductFactory]') AND type in (N'U'))
+DROP TABLE [dbo].[ProductFactory]
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ProductMaterial]') AND type in (N'U'))
+DROP TABLE [dbo].[ProductMaterial]
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ProductMessage]') AND type in (N'U'))
+DROP TABLE [dbo].[ProductMessage]
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ProductOrigin]') AND type in (N'U'))
+DROP TABLE [dbo].[ProductOrigin]
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ProductPackage]') AND type in (N'U'))
+DROP TABLE [dbo].[ProductPackage]
+GO
+
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[TrPlan]') AND type in (N'U'))
+DROP TABLE [dbo].[TrPlan]
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UserGroup]') AND type in (N'U'))
+DROP TABLE [dbo].[UserGroup]
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Video]') AND type in (N'U'))
+DROP TABLE [dbo].[Video]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[User]') AND type in (N'U'))
+DROP TABLE [dbo].[User]
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Vote]') AND type in (N'U'))
+DROP TABLE [dbo].[Vote]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DailyLog]') AND type in (N'U'))
+DROP TABLE [dbo].[DailyLog]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ExamResultsTemp]') AND type in (N'U'))
+DROP TABLE [dbo].[ExamResultsTemp]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Task]') AND type in (N'U'))
+DROP TABLE [dbo].[Task]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Test]') AND type in (N'U'))
+DROP TABLE [dbo].[Test]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MobileUser]') AND type in (N'U'))
+DROP TABLE [dbo].[MobileUser]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[EmpInfo]') AND type in (N'U'))
+DROP TABLE [dbo].[EmpInfo]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Depart_Subject]') AND type in (N'U'))
+DROP TABLE [dbo].[Depart_Subject]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Student]') AND type in (N'U'))
+DROP TABLE [dbo].[Student]
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Team]') AND type in (N'U'))
+DROP TABLE [dbo].[Team]
+GO
+/*É¾³ýTeam¹ØÁªÄÚÈÝ*/
+ALTER TABLE ExamResults DROP COLUMN Team_ID 
+GO
+ALTER TABLE EmpAccount DROP COLUMN Team_ID 
+GO
+ALTER TABLE EmpAccount DROP COLUMN Team_Name 
+GO
