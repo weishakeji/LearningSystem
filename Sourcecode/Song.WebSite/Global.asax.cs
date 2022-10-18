575b0901f3308f6863239a41dc9c16eafef4b872
weishakeji <songleiming@163.com>
2022-10-18 16:08:55 +0800

    修正细节

--- Sourcecode/Song.WebSite/Global.asax.cs
+++ Sourcecode/Song.WebSite/Global.asax.cs
@@ -9,7 +9,7 @@ using System.Web.Routing;
 using Song.ServiceInterfaces;
 using Song.Entities;
 using System.Data;
-using WeiSha.Data;
+//using WeiSha.Data;
 using WeiSha.Core;
 
 namespace Song.WebSite
@@ -18,7 +18,7 @@ namespace Song.WebSite
     {
         protected void Application_Start()
         {
-            WeiSha.Data.Gateway.Default.RegisterLogger(new logger());
+            //WeiSha.Data.Gateway.Default.RegisterLogger(new logger());
 
             AreaRegistration.RegisterAllAreas();
             WebApiConfig.Register(GlobalConfiguration.Configuration);
@@ -40,42 +40,42 @@ namespace Song.WebSite
             //Song.ServiceImpls.AccountLogin.Buffer.Init();
         }
     }
-    /// <summary>
-    /// SQL²éÑ¯¼à¿Ø
-    /// </summary>
-    public class logger : WeiSha.Data.Logger.IExecuteLog
-    {
-        public void Begin(IDbCommand command)
-        {
-            System.Web.HttpContext _context = System.Web.HttpContext.Current;
-            if (_context == null) return;
-            string path = _context.Request.Url.AbsolutePath.Replace("/", "_");
+    ///// <summary>
+    ///// SQL²éÑ¯¼à¿Ø
+    ///// </summary>
+    //public class logger : WeiSha.Data.Logger.IExecuteLog
+    //{
+    //    public void Begin(IDbCommand command)
+    //    {
+    //        System.Web.HttpContext _context = System.Web.HttpContext.Current;
+    //        if (_context == null) return;
+    //        string path = _context.Request.Url.AbsolutePath.Replace("/", "_");
 
-            string sql = command.CommandText;
-            for (int i = 0; i < command.Parameters.Count; i++)
-            {
-                System.Data.SqlClient.SqlParameter para = (System.Data.SqlClient.SqlParameter)command.Parameters[i];
-                string vl = para.Value.ToString();
-                string tp = para.DbType.ToString();
-                if (tp.IndexOf("Int") > -1)
-                    sql = sql.Replace("@p" + i.ToString(), vl);
-                if (tp == "String")
-                    sql = sql.Replace("@p" + i.ToString(), "'" + vl + "'");
-                if (tp == "Boolean")
-                    sql = sql.Replace("@p" + i.ToString(), vl == "True" ? "1" : "0");
-                if (tp == "DateTime")
-                    sql = sql.Replace("@p" + i.ToString(), "'" + ((DateTime)para.Value).ToString("yyyy/MM/dd HH:mm:ss") + "'");
-            }
-            //string t = command.Connection
-            //WeiSha.Core.Log.Info(path, sql);
-            WeiSha.Core.Log.Info(path, sql);
-            //WeiSha.Core.Log.Info(path, command.Connection.ConnectionString);
-            //throw new NotImplementedException();
-        }
+    //        string sql = command.CommandText;
+    //        for (int i = 0; i < command.Parameters.Count; i++)
+    //        {
+    //            System.Data.SqlClient.SqlParameter para = (System.Data.SqlClient.SqlParameter)command.Parameters[i];
+    //            string vl = para.Value.ToString();
+    //            string tp = para.DbType.ToString();
+    //            if (tp.IndexOf("Int") > -1)
+    //                sql = sql.Replace("@p" + i.ToString(), vl);
+    //            if (tp == "String")
+    //                sql = sql.Replace("@p" + i.ToString(), "'" + vl + "'");
+    //            if (tp == "Boolean")
+    //                sql = sql.Replace("@p" + i.ToString(), vl == "True" ? "1" : "0");
+    //            if (tp == "DateTime")
+    //                sql = sql.Replace("@p" + i.ToString(), "'" + ((DateTime)para.Value).ToString("yyyy/MM/dd HH:mm:ss") + "'");
+    //        }
+    //        //string t = command.Connection
+    //        //WeiSha.Core.Log.Info(path, sql);
+    //        WeiSha.Core.Log.Info(path, sql);
+    //        //WeiSha.Core.Log.Info(path, command.Connection.ConnectionString);
+    //        //throw new NotImplementedException();
+    //    }
 
-        public void End(IDbCommand command, ReturnValue retValue, long elapsedTime)
-        {
-            //throw new NotImplementedException();
-        }
-    }
+    //    public void End(IDbCommand command, ReturnValue retValue, long elapsedTime)
+    //    {
+    //        //throw new NotImplementedException();
+    //    }
+    //}
 }
