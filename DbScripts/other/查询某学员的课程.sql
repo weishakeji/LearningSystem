e915572f5275bd10554ab7935ce3c33ef53d717e
weishakeji <10522779@163.com>
2022-10-19 21:03:06 +0800

    修正细节

--- DbScripts/other/查询某学员的课程.sql
+++ DbScripts/other/查询某学员的课程.sql
@@ -1,20 +1,21 @@
+/* 学员的课程，一部分为购买的课程，一部分是所属学员组关联的课程*/
+select * from 
+(
+	select ROW_NUMBER() OVER(Order by Cou_ID) AS 'rowid',*  from 
+	(
+		select cou.* from Course as cou left join  Student_Course as sc
+		on cou.Cou_ID = sc.Cou_ID
+		where sc.Ac_ID=2 and sc.Stc_IsEnable=1 and sc.Stc_Type!=5
+		and (sc.Stc_StartTime<getdate() and  sc.Stc_EndTime>getdate())
+	
+		union
+		--except
 
-select * from (
-select ROW_NUMBER() OVER(Order by Cou_ID) AS 'rowid',*  from (
+		select cou.* from Course as cou right join  StudentSort_Course as ssc
+		on cou.Cou_ID = ssc.Cou_ID
+		where ssc.Sts_ID=2
 
-select cou.* from Course as cou right join  Student_Course as sc
-on cou.Cou_ID = sc.Cou_ID
-where sc.Ac_ID=2 and sc.Stc_IsEnable=1
-and  sc.Stc_EndTime<getdate()  and sc.Stc_Type!=5
---and (sc.Stc_StartTime<getdate() and  sc.Stc_EndTime>getdate())
---order by sc.Stc_EndTime desc
+	) as result where 1=1 or Cou_Name like '%高%' 
+)as t 
 
---union
-except
-
-select cou.* from Course as cou right join  StudentSort_Course as ssc
-on cou.Cou_ID = ssc.Cou_ID
-where ssc.Sts_ID=2
-
-) as result where Cou_Name like '%高%' 
-)as t where  rowid > 1 and rowid<=3
+--where  rowid > 1 and rowid<=3
