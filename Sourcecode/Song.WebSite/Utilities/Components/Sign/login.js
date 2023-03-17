519f28599851c42ba544c56fa6ca12c4f1319b00
weishakeji <10522779@qq.com>
2023-03-17 22:45:30 +0800

    优化：重构学员登录组件，实现滑块拖动的重构

--- Sourcecode/Song.WebSite/Utilities/Components/Sign/login.js
+++ Sourcecode/Song.WebSite/Utilities/Components/Sign/login.js
@@ -13,7 +13,14 @@ Vue.component('login', {
 
             show: false,         //是否显示
             search: '',      //用于检索的字符
-            loading: false
+            acc_form: {
+                acc: '', pwd: ''
+            },
+            acc_vcode: {
+                base64: '', md5: '', val: '', loading: false
+            },
+            loading: false,
+
         }
     },
     watch: {
@@ -22,7 +29,16 @@ Vue.component('login', {
             this.$nextTick(function () {
                 //this.drag();
             });
+        },
+        'acc_form': {
+            handler: function (val, old) {
+                this.$refs['acc_pwd_drag'].init();
+                this.acc_vcode.val = '';
+                this.acc_vcode.md5 = '';
+                this.acc_vcode.base64 = '';
+            }, deep: true
         }
+
     },
     computed: {
 
@@ -31,7 +47,78 @@ Vue.component('login', {
 
     },
     methods: {
+        //账号密码登录
+        submit_accpwd: function (e) {
+            console.error('提交表单');
+            e.preventDefault();
+            //校验输入
+            if (this.acc_form.acc == '') {
+                //alert('账号不得为空');
+            }
+            var th = this;
+            th.loading = true;
+            $api.post('Account/Login', {
+                'acc': th.acc_form.acc, 'pw': th.acc_form.pwd,
+                'vcode': th.acc_vcode.val, 'vmd5': th.acc_vcode.md5
+            }).then(function (req) {
+                //window.login.loading = false;
+                if (req.data.success) {
+                    //登录成功
+                    var result = req.data.result;
+                    $api.loginstatus('account', result.Ac_Pw, result.Ac_ID);
+                    $api.login.account_fresh();
+                    $api.post('Point/AddForLogin', { 'source': '电脑网页', 'info': '账号密码登录', 'remark': '' });
+                    //window.vapp.logged(result);
+                } else {
+                    var data = req.data;
+                    switch (String(data.state)) {
+                        //验证码错误
+                        case '1101':
+                            th.tips('vcode', false, data.message);
+                            break;
+                        case '1102':
+                            th.tips('pwd', false, '账号或密码错误');
+                            break;
+                        case '1103':
+                            th.tips('acc', false, '账号被禁用');
+                            break;
+                        default:
+                            th.tips('acc', false, '登录失败');
+                            console.error(req.data.exception);
+                            console.log(req.data.message);
+                            break;
+                    }
+                }
+            }).catch(err => console.error(err))
+                .finally(() => th.loading = false);
+            return false;
+        },
+        tips:function(prop,success,msg){
 
+        },
+        //滑块拖动完成
+        dragfinish: function () {
+            console.log('拖动完成');
+            //设置验证码输入框为焦点
+            $dom('.login_pwd_area .login_vcode input').focus();
+            var ref = this.$refs;
+            this.getvcode();
+        },
+        //加载验证码图片
+        getvcode: function () {
+            var th = this;
+            th.acc_vcode.loading = true;
+            $api.post('Helper/CheckCodeImg', { 'leng': 4, 'acc': th.acc_form.acc }).then(function (req) {
+                if (req.data.success) {
+                    var result = req.data.result;
+                    th.acc_vcode.base64 = result.base64;
+                    th.acc_vcode.md5 = result.value;
+                } else {
+                    throw req.data.message;
+                }
+            }).catch(err => console.error(err))
+                .finally(() => th.acc_vcode.loading = false);
+        }
     },
     template: `<div class="login_weisha">
         <div class="login_title_bar">
@@ -40,25 +127,35 @@ Vue.component('login', {
                 <span>{{t.name}}</span>
             </div>
         </div>
-        <div class="login_pwd_area" v-if="tabActive=='pwd'">
+        <form class="login_pwd_area" @submit.prevent="submit_accpwd" v-if="tabActive=='pwd'"  remark="账号密码登录">
             <div class="login_acc_pwd">
-                <div><input type="text" tabindex="1" autocomplete="off" placeholder="账号"/></div>
-                <div><input type="password" tabindex="2" autocomplete="off" placeholder="密码"/></div>
+                <div prop="acc">
+                    <input type="text" tabindex="1" autocomplete="off" v-model.trim='acc_form.acc' placeholder="账号"/>
+                    <span class="tips"></span>
+                </div>
+                <div prop="pwd">
+                    <input type="password" tabindex="2" autocomplete="off"  v-model.trim='acc_form.pwd' placeholder="密码"/>
+                    <span class="tips"></span>
+                </div>
             </div>    
-            <div class="login_vcode">
+            <div class="login_vcode"  prop="vcode">
                 <div class="login_vcode_input">
-                    <input type="text" tabindex="3" autocomplete="off" placeholder="校验码"/>
-                    <img class="vcode_img" src=""/>
+                    <input type="number" tabindex="3" v-model.trim='acc_vcode.val' autocomplete="off" placeholder="校验码"/>
+                    <loading bubble v-if="acc_vcode.loading"></loading>
+                    <img v-else class="vcode_img" :src="acc_vcode.base64" :style="{'opacity': acc_vcode.base64=='' ? 0 : 1}"/>
                 </div>               
-                <dragbox id="acc_pwd"></dragbox>
+                <dragbox ref="acc_pwd_drag" id="acc_pwd" @dragfinish="dragfinish"></dragbox>
+                <span class="tips"></span>
             </div> 
             <div class="login_btn">
                 <button type="submit" tabindex="4"><icon>&#xe6c6</icon>登录</button>               
             </div>
-        </div>
-        <div class="login_area" v-if="tabActive=='sms'">
+        </form>
+
+        <div class="login_area" v-if="tabActive=='sms'" remark="短信登录">
             短信的登录
         </div>
+
         <div class="login_register">
             <a href="up" v-if="!config.IsRegStudent"><icon>&#xe7cd</icon>注册</a>
             <a href="find"><icon>&#xe76a</icon>找回密码</a>
@@ -74,28 +171,17 @@ Vue.component('login', {
 
 //滑块
 Vue.component('dragbox', {
-    //id
-    //finish:完成度的值
+    //id: 组件的html元素id
+    //init:完成度的值
     props: ['id'],
     data: function () {
         return {
-            dragvalue: 0,        //当前拖动x坐标值
-            dragper: 0,          //拖动完成度
-
-            finish: 70,
-            //drag_start: false,
-            //drag_end: false,
-
-            //drag_init_x: 0,       //初始坐标
+            finish: 85,         //拖到百分之多少，算完成
             dragging: {
-                init: 0,
-                start: false,
-                end: false,
-                percent: 0,
-                value: 0,
-                finish: false,
-            },
-            //dragfinish: false        //拖动是否完成
+                init: 0, start: false,
+                percent: 0, value: 0,
+                finish: false
+            }
         }
     },
     watch: {
@@ -106,15 +192,35 @@ Vue.component('dragbox', {
                 })
             }, immediate: true
         },
-        'drag_start': function (nv, ov) {
-            console.log(nv);
-        },
+        //拖动中的值变化
         'dragging': {
             handler: function (nv, ov) {
-                console.log(nv);
-                if (!nv.start) {
-                    var dom = $dom('#' + this.dragid);
-                    dom.find('.login_dragbox').left(5);
+                if (!ov) return;
+                //一些计算处理                
+                if (!nv.start && !nv.finish) nv.percent = nv.value = 0;    //当停止拖动时
+                nv.finish = ov.percent >= this.finish;      //是否为完成状态
+                //滑动组件的html元素对象
+                var dom = $dom('#' + this.dragid);
+                var box = dom.find('.login_dragbox');//滑块元素对象
+                var slide = dom.find('.slideway');       //滑块尾部的滑道
+                //如果处在拖动中，则滑块去除动态效果
+                var initleft = 5;
+                if (nv.start) {
+                    box.addClass('drag').css({ 'transition': '', 'opacity': 1 }).left(initleft + nv.value);
+                    slide.css({ 'transition': '' }).show().width(initleft + nv.value);
+                } else {
+                    box.css('transition', 'left 0.3s').removeClass('drag').left(initleft);
+                    slide.css('transition', 'width 0.3s').width(0);
+                }
+                //拖动完成
+                if (nv.finish) {
+                    dom.addClass('complete').css({ 'transition': 'opacity 1s', 'opacity': 0.5 });
+                } else {
+                    dom.removeClass('complete').css({ 'transition': '', 'opacity': 1 }).show();
+                }
+                if (nv.finish && !nv.start) {
+                    this.$emit('dragfinish');
+                    dom.hide();
                 }
             }, immediate: true, deep: true
         },
@@ -124,65 +230,52 @@ Vue.component('dragbox', {
             return 'dragbox_' + this.id;
         }
     },
-    created: function () {
-
-    },
+    created: function () { },
     methods: {
         //滑块拖动
         drag: function (id) {
             var dom = $dom('#' + this.dragid);
+            var box = dom.find('.login_dragbox');       //滑块
             var th = this;
             //滑块的事件;
-            dom.find('.login_dragbox').bind('mousedown,touchstart', function (e) {
+            box.bind('mousedown,touchstart', function (e) {
                 if (e && e.preventDefault) e.preventDefault();
                 if (th.dragging.finish) return;
                 th.dragging.start = true;
-                th.dragging.end = false;
-                th.dragging.finish = false;
                 th.dragging.init = $dom.mouse(e).x; //拖动时的初始鼠标值
             }).bind('mouseup,touchend', function (e) {
                 if (e && e.preventDefault) e.preventDefault();
                 th.dragging.start = false;
-                th.dragging.value = 0;
             });
+            //整个滑动组件的事件
             dom.bind('mouseleave,touchend', function (e) {
                 if (e && e.preventDefault) e.preventDefault();
                 th.dragging.start = false;
-                th.dragging.value = 0;
-            });
-            dom.bind('mousemove,touchmove', function (e) {
+            }).bind('mousemove,touchmove', function (e) {
                 if (e && e.preventDefault) e.preventDefault();
-                if (th.dragging.finish) return;
-                //var obj = login._getObj(e);
-                //if (obj.dragfinish) return; //如果拖动完成，则不能拖动
                 //计算移动最大宽度范围
                 var node = e.target ? e.target : e.srcElement;
                 var parent = $dom(node).parent();
-                var min = 5;
-                var dragbox = dom.find('div.login_dragbox');
-                var max = parent.width() - dragbox.width() - 10;
-                //
-                var mouse = $dom.mouse(e);
-                var left = mouse.x - th.dragging.init;
-                left = left <= min ? min : (left >= max ? max : left);
-                if (th.dragging.start) {
-                    dragbox.left(left);
-                    var perecnt = th.finish;       //允许完成拖动的百分比，即达到这个值就算拖动成功
-                    //console.error(perecnt);
-                    var per = parseInt(dragbox.css('left')) / (parent.width() - dragbox.width()) * 100;
-                    if (per >= perecnt) {
-                        dom.find(".login_drag").addClass('complete').css("opacity", 0.8);
-                    } else {
-                        dom.find(".login_drag").removeClass('complete').css("opacity", 1);
-                    }
-                    th.dragging.percent = per;
-                }
+                var max = parent.width() - box.width() - 15;
+                //计算移动值与完成百分比
+                var left = $dom.mouse(e).x - th.dragging.init;
+                th.dragging.value = left <= 0 ? 0 : (left >= max ? max : left);
+                th.dragging.percent = th.dragging.value / max * 100;
             });
         },
+        //初始化
+        init: function () {
+            this.dragging.start = false;
+            this.dragging.percent = 0;
+            this.dragging.value = 0;
+            this.dragging.finish = false;
+        }
     },
     template: `<div :id="dragid" class="login_drag" v-if="true">
-            <span>向右拖动滑块</span>                                         
-            <div class="login_dragbox"></div>
-            <div class="slideway"></div>
+            <div>
+                <span>向右拖动滑块</span>                                         
+                <div class="login_dragbox"></div>
+                <div class="slideway"></div>
+            </div>
         </div>`
 });
