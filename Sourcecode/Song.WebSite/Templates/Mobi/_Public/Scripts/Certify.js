a8d4dab451c78635c0b4a6922ad4bf992f888b67
weishakeji <10522779@qq.com>
2023-08-16 18:30:32 +0800

    优化：学习证明的手机端，增加学习详情

--- Sourcecode/Song.WebSite/Templates/Mobi/_Public/Scripts/Certify.js
+++ Sourcecode/Song.WebSite/Templates/Mobi/_Public/Scripts/Certify.js
@@ -138,6 +138,12 @@
                 if ((new Date().getFullYear() - date.getFullYear()) > 100) return '';
                 return date.format(fmt);
             },
+            //显示完成度
+            showcomplete: function (num) {
+                num = num > 100 ? 100 : num;
+                num = Math.round(num * 10000) / 10000;
+                return num;
+            },
             //替换星号
             asterisk: function (str, start, end) {
                 var len = str.length;
@@ -162,6 +168,8 @@
                 data: {},        //进度信息         
                 percent: 0,     //完成的真实百分比
                 tolerance: 10,       //容差，例如容差为10，则完成90%即可认为是100%
+                purchase: {},        //购买记录，其中包含学习进度等
+                score: 0,        //最终成绩
                 loading: false
             }
         },
@@ -192,34 +200,62 @@
             onload: function () {
                 var th = this;
                 th.loading = true;
-                $api.cache('Course/LogForVideo:5', { 'couid': this.course.Cou_ID, 'stid': this.stid })
-                    .then(function (req) {
-                        th.loading = false;
-                        if (req.data.success) {
-                            var result = req.data.result;
-                            if (result != null && result.length > 0) {
-                                th.data = result[0];
-                                th.data.lastTime = new Date(th.data.lastTime);
-                                th.percent = th.data.complete;
-                                console.log(th.data);
-                            } else {
-                                th.data = null;
-                                th.percent = 0;
-                            }
-                        } else {
-                            console.error(req.data.exception);
-                            throw req.data.message;
-                        }
-                    }).catch(function (err) {
-                        th.loading = false;
-                        console.error(err);
-                    });
-            }
+                $api.bat(
+                    $api.cache('Course/LogForVideo:5', { 'couid': th.course.Cou_ID, 'stid': th.stid }),
+                    $api.get('Course/Purchaselog', { 'stid': th.stid, 'couid': th.course.Cou_ID }),
+                ).then(axios.spread(function (cou, purchase) {
+                    var result = cou.data.result;
+                    if (result != null && result.length > 0) {
+                        th.data = result[0];
+                        th.data.lastTime = new Date(th.data.lastTime);
+                        th.percent = th.data.complete;
+                        console.log(th.data);
+                    } else {
+                        th.data = null;
+                        th.percent = 0;
+                    }
+                    //购买记录
+                    th.purchase = purchase.data.result;
+                    th.score = th.resultScore(purchase.data.result).score;
+                })).catch(err => console.error(err))
+                    .finally(() => th.loading = false);
+            },
+            //综合得分 purchase：课程购买记录（记录中包含学习进度等信息）
+            resultScore: function (purchase) {
+                var th = this;
+                //视频得分
+                var weight_video = orgconfig('finaltest_weight_video', 33.3);
+                //加上容差
+                var video = purchase.Stc_StudyScore > 0 ? purchase.Stc_StudyScore + orgconfig('VideoTolerance', 0) : 0;
+                video = video >= 100 ? 100 : video;
+                video = weight_video * video / 100;
+                //试题得分
+                var weight_ques = orgconfig('finaltest_weight_ques', 33.3);
+                var ques = weight_ques * purchase.Stc_QuesScore / 100;
+                //结考课试分
+                var weight_exam = orgconfig('finaltest_weight_exam', 33.3);
+                var exam = weight_exam * purchase.Stc_ExamScore / 100;
+                //最终得分
+                var score = Math.round((video + ques + exam) * 100) / 100;
+                score = score >= 100 ? 100 : score;
+                return {
+                    'video': video,
+                    'ques': ques,
+                    'exam': exam,
+                    'score': score
+                }
+                //获取机构的配置参数
+                function orgconfig(para, def) {
+                    var val = Number(th.config[para]);
+                    if (isNaN(val)) return def ? def : '';
+                    return val;
+                };
+            },
         },
         // 同样也可以在 vm 实例中像 "this.message" 这样使用
         template: `<span>
                 <loading v-if="loading"></loading>
-                <slot v-else :value='progress' :course='data'></slot>
+                <slot v-else :value='progress' :score="score" :course='data' :purchase="purchase"></slot>
         </span>`
     });
 });
