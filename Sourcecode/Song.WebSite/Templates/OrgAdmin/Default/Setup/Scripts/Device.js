b675bd8338a0da84c097eccd13ca185446fa4394
weishakeji <10522779@qq.com>
2023-10-03 17:43:22 +0800

    优化：机构管理中的“学习相关”、“设备终端”管理的页面重构

---
+++ Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/Setup/Scripts/Device.js
@@ -0,0 +1,54 @@
+$ready(function () {
+
+    window.vapp = new Vue({
+        el: '#vapp',
+        data: {
+            organ: {},
+            config: {},      //当前机构配置项
+            rules: {},
+            loading: false
+        },
+        mounted: function () {
+            var th = this;
+            th.loading = true;
+            $api.bat(
+                $api.get('Organization/Current')
+            ).then(axios.spread(function (organ) {
+                //获取结果             
+                th.organ = organ.data.result;
+                //机构配置信息
+                th.config = $api.organ(th.organ).config;
+            })).catch(err => console.error(err))
+                .finally(() => th.loading = false);
+        },
+        created: function () { },
+        computed: {},
+        watch: {},
+        methods: {
+            btnEnter: function (formName) {
+                //仅保留当前页要修改的字段
+                let fields = this.$refs[formName].fields;
+                let data = {};      //          
+                for (let i = 0; i < fields.length; i++)
+                    data[fields[i].prop] = this.config[fields[i].prop];
+                //保存
+                var th = this;
+                th.loading = true;
+                $api.post('Organization/ConfigUpdate', {
+                    "orgid": th.organ.Org_ID, 'config': data
+                }).then(function (req) {
+                    if (req.data.success && req.data.result) {
+                        th.$message({
+                            type: 'success', center: true,
+                            message: '操作成功!'
+                        });
+                    } else {
+                        throw req.data.message;
+                    }
+                }).catch(err => alert(err, '错误'))
+                    .finally(() => th.loading = false);
+            },
+        }
+    });
+
+});
---
+++ Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/Setup/Scripts/Study.js
@@ -0,0 +1,116 @@
+$ready(function () {
+
+    window.vapp = new Vue({
+        el: '#vapp',
+        data: function () {
+            var validatePass = function (rule, value, callback) {
+                var th = vapp;
+                //结课成绩的权重
+                var finaltest_weight_video = !!th.config.finaltest_weight_video ? th.config.finaltest_weight_video : 0;
+                var finaltest_weight_ques = !!th.config.finaltest_weight_ques ? th.config.finaltest_weight_ques : 0;
+                var finaltest_weight_exam = !!th.config.finaltest_weight_exam ? th.config.finaltest_weight_exam : 0;
+                var finaltest_score = finaltest_weight_video + finaltest_weight_ques + finaltest_weight_exam;
+                if (finaltest_score != 100) {
+                    callback(new Error('各项权重必须合计等于100%'));
+                    return false;
+                } else {
+                    callback();
+                }
+            }
+            return {
+                organ: {},
+                config: {},      //当前机构配置项
+                rules: {
+                    'finaltest_weight_video': [
+                        { required: true, message: '不得为空', trigger: 'blur' },
+                        { validator: validatePass, trigger: 'blur' }
+                    ],
+                    'finaltest_weight_ques': [
+                        { required: true, message: '不得为空', trigger: 'blur' },
+                        { validator: validatePass, trigger: 'blur' }
+                    ],
+                    'finaltest_weight_exam': [
+                        { required: true, message: '不得为空', trigger: 'blur' },
+                        { validator: validatePass, trigger: 'blur' }
+                    ],
+                    'finaltest_score_pass': [
+                        { required: true, message: '及格分不得为空', trigger: 'blur' }
+                    ],
+                },
+                loading: false
+            };
+        },
+
+        mounted: function () {
+            var th = this;
+            th.loading = true;
+            $api.bat(
+                $api.get('Organization/Current')
+            ).then(axios.spread(function (organ) {
+                //获取结果             
+                th.organ = organ.data.result;
+                //机构配置信息
+                th.config = $api.organ(th.organ).config;
+            })).catch(err => console.error(err))
+                .finally(() => th.loading = false);
+        },
+        created: function () { },
+        computed: {},
+        watch: {},
+        methods: {
+            validatePass: function (rule, value, callback) {
+                var th = vapp;
+                //结课成绩的权重
+                var finaltest_weight_video = !!th.config.finaltest_weight_video ? th.config.finaltest_weight_video : 0;
+                var finaltest_weight_ques = !!th.config.finaltest_weight_ques ? th.config.finaltest_weight_ques : 0;
+                var finaltest_weight_exam = !!th.config.finaltest_weight_exam ? th.config.finaltest_weight_exam : 0;
+                var finaltest_score = finaltest_weight_video + finaltest_weight_ques + finaltest_weight_exam;
+                if (finaltest_score != 100) {
+                    callback(new Error('各项权重必须合计等于100%'));
+                    return false;
+                } else {
+                    callback();
+                }
+
+                return true;
+                return;
+                if (value === '') {
+                    callback(new Error('请输入密码'));
+                } else {
+                    if (this.ruleForm.checkPass !== '') {
+                        this.$refs.ruleForm.validateField('checkPass');
+                    }
+                    callback();
+                }
+            },
+            btnEnter: function (formName) {
+                var th = this;
+                this.$refs[formName].validate((valid, fields) => {
+                    if (valid) {
+                        //仅保留当前页要修改的字段
+                        let fields = th.$refs[formName].fields;
+                        let data = {};      //          
+                        for (let i = 0; i < fields.length; i++)
+                            data[fields[i].prop] = this.config[fields[i].prop];
+                        //保存             
+                        th.loading = true;
+                        $api.post('Organization/ConfigUpdate', {
+                            "orgid": th.organ.Org_ID, 'config': data
+                        }).then(function (req) {
+                            if (req.data.success && req.data.result) {
+                                th.$message({
+                                    type: 'success', center: true,
+                                    message: '操作成功!'
+                                });
+                            } else {
+                                throw req.data.message;
+                            }
+                        }).catch(err => alert(err, '错误'))
+                            .finally(() => th.loading = false);
+                    }
+                });
+            },
+        }
+    });
+
+});
