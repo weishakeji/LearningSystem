63cb83bb9fd3879edee57c0b39551ae319841d3b
weishakeji <10522779@qq.com>
2023-10-24 21:44:18 +0800

    修正细节

--- Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/Data/Scripts/VideoProgress.js
+++ Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/Data/Scripts/VideoProgress.js
@@ -2,168 +2,20 @@ $ready(function () {
     window.vapp = new Vue({
         el: '#vapp',
         data: {
-            form: {
-                orgid: '', use: '', show: '', search: '',
-                size: 20, index: 1
-            },
-            organ: {},       //当前机构
-            config: {},
-
-            loading: false,
-            loadingid: 0,        //当前操作中的对象id
-            datas: [], //数据源           
-
-            total: 1, //总记录数
-            totalpages: 1, //总页数
-            selects: [] //数据表中选中的行
+          
         },
         watch: {
-            'loading': function (val, old) {
-                //console.log(val);
-            }
+            
         },
         computed: {
 
         },
         created: function () {
-            var th = this;
-            $api.bat(
-                $api.get('Organization/Current')
-            ).then(axios.spread(function (organ) {
-                //获取结果             
-                th.organ = organ.data.result;
-                th.form.orgid = th.organ.Org_ID;
-                //机构配置信息
-                th.config = $api.organ(th.organ).config;
-                th.handleCurrentChange(1);
-            })).catch(err => console.error(err))
-                .finally(() => th.loading_init = false);
+           
         },
         methods: {
-            //加载数据页
-            handleCurrentChange: function (index) {
-                if (index != null) this.form.index = index;
-                var th = this;
-                //每页多少条，通过界面高度自动计算
-                var area = document.documentElement.clientHeight - 100;
-                th.form.size = Math.floor(area / 67);
-                th.loading = true;
-                $api.cache("Link/SortPager:update", th.form).then(function (d) {
-                    th.loading = false;
-                    if (d.data.success) {
-                        th.datas = d.data.result;
-                        th.totalpages = Number(d.data.totalpages);
-                        th.total = d.data.total;
-                    } else {
-                        throw d.data.message;
-                    }
-                    th.rowdrop();
-                }).catch(err => console.error(err))
-                    .finally(() => th.loading = false);
-            },
-            //删除
-            deleteData: function (datas) {
-                var th = this;
-                th.loading = true;
-                $api.delete('Link/SortDelete', { 'id': datas }).then(function (req) {
-                    if (req.data.success) {
-                        var result = req.data.result;
-                        th.$notify({
-                            type: 'success',
-                            message: '成功删除' + result + '条数据',
-                            center: true
-                        });
-                        th.handleCurrentChange();
-                    } else {
-                        console.error(req.data.exception);
-                        throw req.data.message;
-                    }
-                }).catch(function (err) {
-                    alert(err, '错误');
-                    console.error(err);
-                }).finally(() => th.loading = false);
-            },
-            //更改使用状态
-            changeState: function (row) {
-                var th = this;
-                this.loadingid = row.Ls_Id;
-                $api.post('Link/SortModify', { 'entity': row }).then(function (req) {
-                    if (req.data.success) {
-                        th.$notify({
-                            type: 'success',
-                            message: '修改状态成功!',
-                            center: true
-                        });
-                    } else {
-                        throw req.data.message;
-                    }
-                }).catch(function (err) {
-                    th.$alert(err, '错误');
-                }).finally(() => th.loadingid = 0);
-            },
-            //行的拖动
-            rowdrop: function () {
-                // 首先获取需要拖拽的dom节点            
-                const el1 = document.querySelectorAll('table > tbody')[0];
-                if (el1 == null) return;
-                Sortable.create(el1, {
-                    disabled: false, // 是否开启拖拽
-                    ghostClass: 'sortable-ghost', //拖拽样式
-                    handle: '.draghandle',     //拖拽的操作元素
-                    animation: 150, // 拖拽延时，效果更好看
-                    group: { // 是否开启跨表拖拽
-                        pull: false,
-                        put: false
-                    },
-                    onStart: function (evt) {
-                    },
-                    onMove: function (evt, originalEvent) {
-                        if ($dom('table tr.expanded').length > 0) {
-                            return false;
-                        };
-
-                        evt.dragged; // dragged HTMLElement
-                        evt.draggedRect; // TextRectangle {left, top, right и bottom}
-                        evt.related; // HTMLElement on which have guided
-                        evt.relatedRect; // TextRectangle
-                        originalEvent.clientY; // mouse position
-
-                    },
-                    onEnd: (e) => {
-                        let arr = this.datas; // 获取表数据
-                        arr.splice(e.newIndex, 0, arr.splice(e.oldIndex, 1)[0]); // 数据处理
-                        this.$nextTick(function () {
-                            this.datas = arr;
-                            for (var i = 0; i < this.datas.length; i++) {
-                                this.datas[i].Ls_Tax = (this.form.size * (this.form.index - 1)) + i + 1;
-                            }
-                            this.changeTax();
-                        });
-                    }
-                });
-            },
-            //更新排序
-            changeTax: function () {
-                var arr = $api.clone(this.datas);
-                for (var i = 0; i < arr.length; i++)
-                    delete arr[i]['childs'];
-                var th = this;
-                $api.post('Link/SortUpdateTaxis', { 'items': arr }).then(function (req) {
-                    if (req.data.success) {
-                        th.$notify({
-                            type: 'success',
-                            message: '修改顺序成功!',
-                            center: true
-                        });
-                    } else {
-                        throw req.data.message;
-                    }
-                }).catch(function (err) {
-                    alert(err);
-                    console.error(err);
-                });
-            }
+           
         }
     });
 
-}, ['Components/links_count.js']);
+}, []);
