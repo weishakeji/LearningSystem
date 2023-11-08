9d05b9359046d999f8448e145167b541300d45f3
weishakeji <10522779@qq.com>
2023-11-08 16:44:33 +0800

    修正：工具栏按钮无法响应不带图标样式的传值

--- Sourcecode/Song.WebSite/Utilities/Scripts/vuecomponent.js
+++ Sourcecode/Song.WebSite/Utilities/Scripts/vuecomponent.js
@@ -51,12 +51,12 @@ Vue.component('btngroup', {
     computed: {
         buttons: function () {
             if (this.show == '') return [];
-            var btnarr = [];
+            let btnarr = [];
             //按参数show的设置顺序来显示按钮
-            var showarr = this.show.split(',');
-            for (var i = 0; i < showarr.length; i++) {
+            let showarr = this.show.split(',');
+            for (let i = 0; i < showarr.length; i++) {
                 const show = showarr[i];
-                for (var j = 0; j < this.buttonArray.length; j++) {
+                for (let j = 0; j < this.buttonArray.length; j++) {
                     const btn = this.buttonArray[j];
                     if (show == btn.text) {
                         btnarr.push(btn);
@@ -65,10 +65,10 @@ Vue.component('btngroup', {
                 }
             }
             //如果按钮属性为enable，不用在show中设置也可以显示
-            for (var i = 0; i < this.buttonArray.length; i++) {
+            for (let i = 0; i < this.buttonArray.length; i++) {
                 const btn = this.buttonArray[i];
-                var isexist = false;
-                for (var j = 0; j < btnarr.length; j++) {
+                let isexist = false;
+                for (let j = 0; j < btnarr.length; j++) {
                     if (btnarr[j].text == btn.text) {
                         isexist = true;
                         break;
@@ -86,13 +86,13 @@ Vue.component('btngroup', {
             this.buttonArray.push(btn);
         },
         eventClick: function (btn) {
-            var btnid = btn.id;
+            let btnid = btn.id;
             //当前点击的按钮
-            var curr = this.getCurrbtn(btnid);
+            let curr = this.getCurrbtn(btnid);
             //新增与修改因为有默认事件方法，所有这里做个判断         
             if (btnid == 'add' || btnid == 'modify') {
                 //如果设置了事件，则直接执行，否则执行默认事件
-                var existEvent = this.$listeners[btnid];
+                let existEvent = this.$listeners[btnid];
                 if (existEvent) return this.$emit(btnid, curr, this);
                 if (!top.$pagebox) {
                     return this.$message({
@@ -105,7 +105,7 @@ Vue.component('btngroup', {
             if (btnid == 'modify') return this.modify(this.getid());       //修改            
             if (btnid == 'delete') return this.delete(this.getids(), curr);    //删除   
             //其它按钮事件
-            var existEvent = this.$listeners[btnid];
+            let existEvent = this.$listeners[btnid];
             if (existEvent) return this.$emit(btnid, curr, this.getids());
         },
         //添加按钮事件
@@ -137,17 +137,17 @@ Vue.component('btngroup', {
                 return;
             }
             if (!(top.$pagebox && this.path)) return;
-            var url = this.setParameter(this.path, id);
+            let url = this.setParameter(this.path, id);
             this.pagebox(url, title ? title : '修改', window.name + '_' + id + '[modify]', this.width, this.height, param);
         },
         modifyrow: function (row, title, param) {
             if (!this.idkey) return '';
-            var id = !!row[this.idkey] ? row[this.idkey] : '';
+            let id = !!row[this.idkey] ? row[this.idkey] : '';
             this.modify(id, title, param);
         },
         //删除事件
         delete: function (ids, btn) {
-            var arr = String(ids).split(',');
+            let arr = String(ids).split(',');
             if (ids == '' || arr.length < 1) {
                 this.$message({
                     message: '请选中要操作的数据行',
@@ -170,9 +170,9 @@ Vue.component('btngroup', {
         },
         //获取当前点击的按钮
         getCurrbtn: function (btnid) {
-            var curr = {};
-            var btn = this.buttons;
-            for (var t in btn) {
+            let curr = {};
+            let btn = this.buttons;
+            for (let t in btn) {
                 if (btn[t].id == btnid) {
                     curr = btn[t];
                     break;
@@ -183,21 +183,21 @@ Vue.component('btngroup', {
         //获取id
         getid: function () {
             if (!this.idkey || !this.selects || this.selects.length < 1) return '';
-            var id = !!this.selects[0][this.idkey] ? this.selects[0][this.idkey] : '';
+            let id = !!this.selects[0][this.idkey] ? this.selects[0][this.idkey] : '';
             return id;
         },
         //获取树菜单节点数据
         getnode: function () {
-            var tree = top.tree;
+            let tree = top.tree;
             if (tree) return tree.getData(window.name);
             return null;
         },
         //获取多个id，用逗号分隔
         getids: function () {
             if (!this.idkey || !this.selects || this.selects.length < 1) return '';
-            var str = '';
-            for (var i = 0; i < this.selects.length; i++) {
-                var id = !!this.selects[i][this.idkey] ? this.selects[i][this.idkey] : '';
+            let str = '';
+            for (let i = 0; i < this.selects.length; i++) {
+                let id = !!this.selects[i][this.idkey] ? this.selects[i][this.idkey] : '';
                 if (id == '') continue;
                 str += i < this.selects.length - 1 ? id + ',' : id;
             }
@@ -206,13 +206,13 @@ Vue.component('btngroup', {
         //获取完整的页面路径
         getfullpath: function (path) {
             if (path.substring(0, 1) == "/") return path;
-            var root = String(window.document.location.href);
-            var file = root.substring(0, root.lastIndexOf("/") + 1);
+            let root = String(window.document.location.href);
+            let file = root.substring(0, root.lastIndexOf("/") + 1);
             return file + path;
         },
         //设置参数
         setParameter: function (url, id) {
-            var params = $api.url.params();
+            let params = $api.url.params();
             for (let i = 0; i < params.length; i++)
                 url = $api.url.set(url, params[i].key, params[i].val);
             return $api.url.set(url, 'id', id);
@@ -230,29 +230,32 @@ Vue.component('btngroup', {
                 });
             }
             url = this.getfullpath(url);
-            var node = this.getnode();
-            var tit = node ? node.title : $dom('title').text();
+            let node = this.getnode();
+            let tit = node ? node.title : $dom('title').text();
             if ($dom('title').text() != '' && (!title || title != '')) tit += ' - ';
-            var ico = this.ico ? this.ico : (node && node.ico != '' ? node.ico : 'a021');           
-            var attrs = {
+            let ico = this.ico ? this.ico : (node && node.ico != '' ? node.ico : 'a021');
+            let iconstyle = node && !!node.icon ? node.icon : {};
+            let titstyle = node && !!node.font ? node.font : {};
+            let attrs = {
                 width: width ? width : 400,
                 height: height ? height : 300,
                 url: url,
-                ico: ico, iconstyle: node.icon,
+                ico: ico, iconstyle: iconstyle,
                 pid: window.name,
                 id: boxid,
-                title: tit + title, titstyle: node.font
+                title: tit + title, titstyle: titstyle
             };
             if (param != null) {
-                for (var t in param) attrs[t] = param[t];
+                for (let t in param) attrs[t] = param[t];
             }
+            //如果是模态窗口，则显示遮罩且不允许最小化
             if (this.modal) {
                 attrs['showmask'] = true;
                 attrs['min'] = false;
             }
             //console.log(attrs);
             if (!top.$pagebox) return;
-            var pbox = top.$pagebox.create(attrs);
+            let pbox = top.$pagebox.create(attrs);
             pbox.open();
         },
         //按钮图标
