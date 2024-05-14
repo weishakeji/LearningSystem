cb88cb447532b78bb3cb2ce23af8e82c6961ef83
weishakeji <10522779@qq.com>
2024-05-14 16:58:16 +0800

    优化：编写机构管理欢迎页的“热门课程”

---
+++ Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/Viewport/Components/courses_hot.js
@@ -0,0 +1,66 @@
+﻿
+//热门课程
+Vue.component('courses_hot', {
+    //sbjid:专业id
+    //couid:当前课程，在获取的课程中去除该课程
+    props: ["sbjid", "couid", "org", "count"],
+    data: function () {
+        return {
+            show: false,         //是否显示，  
+            datas: [],
+            loading: false,
+        }
+    },
+    watch: {
+        'org': {
+            handler: function (nv, ov) {
+                if ($api.isnull(nv)) return;
+                this.getcourses(nv.Org_ID, this.count);
+            }, immediate: true
+        }
+    },
+    computed: {},
+    mounted: function () {
+        var css = $dom.path() + 'Viewport/Components/Styles/courses_hot.css';
+        $dom.load.css([css]);
+    },
+    methods: {
+        //跳转
+        godetail: function (id) {
+            return $api.url.dot(id, '/web/Course/Detail');
+        },
+        //从指专业内获取课程列表
+        getcourses: function (orgid, count) {
+            var th = this;
+            th.loading = true;
+            var form = {
+                'orgid': orgid, 'sbjid': '', 'start': '', 'end': '', 'size': count, 'index': 1
+            };
+            $api.get('Course/MostHot', form)
+                .then(function (req) {
+                    if (req.data.success) {
+                        th.datas = req.data.result;
+                    } else {
+                        console.error(req.data.exception);
+                        throw req.config.way + ' ' + req.data.message;
+                    }
+                }).catch(err => console.error(err))
+                .finally(() => th.loading = false);
+        }
+    },
+
+    template: `<weisha_courses>  
+        <loading v-if="loading">loading ... </loading>        
+        <slot v-else v-for="(cour,index) in datas" name="item" :data="cour" :index="index">
+            <div :rec="cour.Cou_IsRec" :class="{'rightmost':(index+1)%4==0}">
+                <a :href="godetail(cour.Cou_ID)" target="_blank">
+                    <img :src="cour.Cou_LogoSmall" v-if="cour.Cou_LogoSmall && cour.Cou_LogoSmall!=''"/>
+                    <img src="/Utilities/images/cou_nophoto.jpg" v-else />
+                </a>
+                <el-tag type="warning" class="type" v-if="cour.Cou_Type">试题库</el-tag>                  
+                <a :href="godetail(cour.Cou_ID)" target="_blank" class="name">{{index+1}}. {{ cour.Cou_Name }}</a>
+                <el-tag type="warning" class="view" title="选修人次"><icon student></icon>{{cour.Cou_TryNum}}</el-tag>              
+            </div>       
+        </slot>        
+    </weisha_courses>`
+});
--- Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/Viewport/Components/piece.js
+++ Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/Viewport/Components/piece.js
@@ -7,7 +7,9 @@ Vue.component('piece', {
         "ico",          //右侧大图标
         "iconsize",     //图标的大小，默认是100像素
         "leftcolor",        //左侧背景色，背景是过渡色
-        "rightcolor"],      //右侧背景色
+        "rightcolor"         //右侧背景色
+       
+    ],
     data: function () {
         return {}
     },
@@ -31,17 +33,22 @@ Vue.component('piece', {
             let left = $api.isnull(this.leftcolor) ? '#fff' : this.leftcolor;
             let right = $api.isnull(this.rightcolor) ? '#fff' : this.rightcolor;
             return 'background: linear-gradient(to right, ' + left + ', ' + right + ');';
+        },
+        //字体颜色
+        "font_color":function(){
+            if ($api.isnull(this.leftcolor)) return '';        
+            return 'color: ' + this.leftcolor + ';';
         }
     },
     mounted: function () {
     },
     methods: {
     },
-    template: `<div class="piece" :style="background_color">        
+    template: `<div class="piece" :style="background_color">    
                 <icon class="bg" v-html="background_ico" v-if="background_ico!=''" :style="background_size"></icon>              
                 <div class="area">
                     <div class="tit">{{title}}</div>
-                    <div class="contx">
+                    <div class="contx" :style="font_color">
                         <slot></slot>
                     </div>
                 </div>
