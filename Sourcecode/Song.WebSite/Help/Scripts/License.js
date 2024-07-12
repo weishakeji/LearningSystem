0e531a9c33f23c7cca263a836ab47ef9a9c3c988
weishakeji <10522779@qq.com>
2024-07-12 18:09:03 +0800

    优化帮助中心

---
+++ Sourcecode/Song.WebSite/Help/Scripts/License.js
@@ -0,0 +1,34 @@
+﻿
+window.vapp = new Vue({
+    el: '#vapp',
+    data: {
+        //版本信息
+        editions: [],
+        loading: false
+    },
+    computed: {
+
+    },
+    watch: {
+
+    },
+    mounted: function () {
+        this.getEdition();
+    },
+    methods: {
+        getEdition: function () {
+            var th = this;
+            th.loading = true;
+            $api.get('Platform/EditionsChinese').then(req => {
+                if (req.data.success) {
+                    th.editions = req.data.result;
+                } else {
+                    console.error(req.data.exception);
+                    throw req.config.way + ' ' + req.data.message;
+                }
+            }).catch(err => console.error(err))
+                .finally(() => th.loading = false);
+        }
+    },
+
+});
