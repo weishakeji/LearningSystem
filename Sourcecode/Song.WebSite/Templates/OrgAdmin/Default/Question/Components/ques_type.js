fec67b31787125be39d77ad30dcd5cd695647524
weishakeji <10522779@qq.com>
2024-09-24 10:14:51 +0800

    修正细节

--- Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/Question/Components/ques_type.js
+++ Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/Question/Components/ques_type.js
@@ -39,7 +39,7 @@ Vue.component('ques_type', {
     template: `<span>
         <loading star v-if="isnull || typeindex==0"></loading>
         <template v-else>
-            <icon v-if="showicon" ques_type v-html="seticon(typeindex)" :title="settitle(typeindex)" :showname="showname"></icon> 
+            <icon v-if="showicon || !showname" ques_type v-html="seticon(typeindex)" :title="settitle(typeindex)" :showname="showname"></icon> 
             <span v-else :title="settitle(typeindex)" :showname="showname">{{settitle(typeindex)}}</span>
         </template>
     </span>`
