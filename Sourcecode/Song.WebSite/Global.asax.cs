e38c1961fa262970ec01e79a3c625069a14de061
weishakeji <10522779@qq.com>
2024-12-19 10:43:19 +0800

    优化了定时任务

--- Sourcecode/Song.WebSite/Global.asax.cs
+++ Sourcecode/Song.WebSite/Global.asax.cs
@@ -10,6 +10,9 @@ using Song.ServiceInterfaces;
 using Song.Entities;
 using System.Data;
 using WeiSha.Core;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
 
 namespace Song.WebSite
 {
@@ -46,11 +49,61 @@ namespace Song.WebSite
             {
                 Log.Error(this.GetType().ToString(), ex);
             }
-            //更新统计数据,延迟执行
-            WeiSha.Core.Business.Do<IOrganization>().UpdateStatisticalData_Delay(10);
-            //创建定时任务
-            WeiSha.Core.Business.Do<IOrganization>().UpdateStatisticalData_CronJob();
-            
+            ////更新统计数据,延迟执行
+            //WeiSha.Core.Business.Do<IOrganization>().UpdateStatisticalData_Delay(10);
+            ////创建定时任务
+            //WeiSha.Core.Business.Do<IOrganization>().UpdateStatisticalData_CronJob();
+
+            //执行定时任务
+            // 启动 Quartz 调度器  
+            StartScheduler().GetAwaiter().GetResult();
+
+        }
+        // 定时任务的调度器
+        private static IScheduler _scheduler;
+        private async Task StartScheduler()
+        {
+            // 创建调度器  
+            _scheduler = await StdSchedulerFactory.GetDefaultScheduler();
+
+
+            // 定义作业  
+            IJobDetail job = JobBuilder.Create<ScheduledTaskJob>()
+                .WithIdentity("ScheduledTaskJob", "group1")
+                .Build();
+
+            //定时的时间，CRON 表达式
+            string cron = WeiSha.Core.App.Get["QueryDetail_Cron"].String;
+            // 创建触发器，设置指定时间执行  
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("myTrigger", "group1")
+                .StartNow()
+                .WithCronSchedule(cron) // CRON 表达式
+                .Build();
+
+            // 调度作业  
+            await _scheduler.ScheduleJob(job, trigger);
+
+            await _scheduler.Start();
+        }
+        protected void Application_End(object sender, EventArgs e)
+        {
+            // 停止并释放 Quartz 调度器  
+            _scheduler?.Shutdown(waitForJobsToComplete: true);
+        }
+       
+    }
+    /// <summary>
+    /// 定时任务的执行类
+    /// </summary>
+    public class ScheduledTaskJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            //执行方法          
+            //更新统计数据
+            WeiSha.Core.Business.Do<IOrganization>().UpdateStatisticalData();
+            await Task.CompletedTask;
         }
     }
 }
