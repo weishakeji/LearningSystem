0253c22c53ef9c2e6d3a85247f3411d03037c6e3
weishakeji <10522779@qq.com>
2025-05-15 22:27:38 +0800

    修正细节

--- DbScripts/upgrade_v2/2025-05-07.sql
+++ DbScripts/upgrade_v2/2025-05-07.sql
@@ -1,2 +1,3 @@
-/* 修正试题的难度等级，不知道为什么会出现大于5的情况 */
-UPDATE "Questions" SET "Qus_Diff"=3  WHERE "Qus_Diff"<1 or "Qus_Diff">5
+/* 修正试题的难度等级，不知道为什么试题难度会出现大于5的情况 */
+UPDATE "Questions" SET "Qus_Diff"=5  WHERE "Qus_Diff">=5;
+UPDATE "Questions" SET "Qus_Diff"=1  WHERE "Qus_Diff"<=1;
