a8eb0dadeda7ee6c1f1b6d7594e9c86eac09b855
weishakeji <10522779@qq.com>
2025-06-09 00:22:46 +0800

    编写大语言模型的配置项，在超管那里

---
+++ Sourcecode/Song.WebSite/Templates/Manage/Default/Setup/Scripts/LargeLanguageModel.js
@@ -0,0 +1,78 @@
+﻿
+$ready(function () {
+    window.vapp = new Vue({
+        el: '#vapp',
+        data: {
+            //要操作的对象
+            entity: { 'Sys_Key': 'LLM_aliyun_APIKey', 'Sys_Value': '', 'Sys_Id': 0 },
+            rules: {
+                'Sys_Value': [
+                    { required: true, message: '请输入阿里云百炼平台的AppKey', trigger: 'blur' }
+                ]
+            },
+            error: '',       //错误提示
+            //当前登录的管理员
+            admin: null,
+            //当前机构
+            org: {},
+            loading: false,
+            loading_init: false
+        },
+        computed: {
+            //是否登录
+            islogin: (t) => { return !$api.isnull(t.admin); },
+        },
+        mounted: function () {
+            var th = this;
+            th.loading_init = true;
+            $api.get('Admin/Super').then(function (req) {
+                if (req.data.success) {
+                    th.admin = req.data.result;
+                    if (!th.islogin) return;
+                    th.loading = true;
+                    $api.bat(
+                        $api.get('Platform/ParamForKey', { 'key': th.entity.Sys_Key }),
+                        $api.get('Organization/ForID', { 'id': th.admin.Org_ID })
+                    ).then(([param, org]) => {
+                        if (param.data.result)
+                            th.entity = param.data.result;
+                        th.org = org.data.result;
+                    }).catch(err => alert(err))
+                        .finally(() => th.loading = false);
+                } else {
+                    throw req.config.way + ' ' + req.data.message;
+                }
+            }).catch(err => console.error(err))
+                .finally(() => th.loading_init = false);
+        },
+        created: function () {
+
+        },
+        methods: {
+            //保存信息
+            btnEnter: function () {
+                var th = this;
+                if (th.loading) return;
+                this.$refs['entity'].validate(function (valid) {
+                    if (valid) {
+                        th.loading = true;
+                        $api.post("Platform/ParamModify", { 'entity': th.entity }).then(function (req) {
+                            if (req.data.success) {
+                                th.entity = req.data.result;
+                                th.$message({ message: '操作成功', type: 'success' });
+                            } else {
+                                console.error(req.data.exception);
+                                throw req.config.way + ' ' + req.data.message;
+                            }
+                        }).catch(function (err) {
+                            alert(err);
+                        }).finally(() => th.loading = false);
+                    }
+                });
+            },
+        }
+    });
+
+});
+
+
