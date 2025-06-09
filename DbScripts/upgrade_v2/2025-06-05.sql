9d4f4ed317fcffb09b80b068475f4b84e6c707a1
weishakeji <10522779@qq.com>
2025-06-09 20:04:36 +0800

    生成初始SQL脚本

--- DbScripts/upgrade_v2/2025-06-05.sql
+++ DbScripts/upgrade_v2/2025-06-05.sql
@@ -18,3 +18,8 @@ CREATE INDEX IF NOT EXISTS "LlmRecords_aaaaaLlmRecords_PK" ON "LlmRecords" ("Llr
 CREATE INDEX IF NOT EXISTS "LlmRecords_IX_Llr_CrtTime" ON "LlmRecords" ("Llr_CrtTime" ASC);
 CREATE INDEX IF NOT EXISTS "LlmRecords_IX_Llr_LastTimee" ON "LlmRecords" ("Llr_LastTime" ASC);
 CREATE INDEX IF NOT EXISTS "LlmRecords_IX_Ac_ID" ON "LlmRecords" ("Ac_ID" ASC);
+
+
+/*添加大语言模型的菜单项*/
+INSERT INTO "ManageMenu"("MM_AbbrName","MM_Color","MM_Complete","MM_Font","MM_Func","MM_Help","MM_IcoCode","MM_IcoColor","MM_IcoSize","MM_IcoX","MM_IcoY","MM_Intro","MM_IsBold","MM_IsChilds","MM_IsFixed","MM_IsItalic","MM_IsShow","MM_IsUse","MM_Link","MM_Marker","MM_Name","MM_PatId","MM_Root","MM_Tax","MM_Type","MM_UID","MM_WinHeight","MM_WinID","MM_WinMax","MM_WinMin","MM_WinMove","MM_WinResize","MM_WinWidth") VALUES ('','',100,'','func','','e820','',5,0,0,'',false,false,false,false,true,true,'/manage/setup/LargeLanguageModel','','大语言模型','742f03375a49149ef533668189ec0777',88,6,'item','1749396749753',0,'',false,false,false,false,0);
+
