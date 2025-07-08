edd42c2af82e64cfc4faf0d23917a44f44ddeddc
weishakeji <10522779@qq.com>
2025-07-08 23:34:15 +0800

    优化：调整机构管理的后台样式，隐藏系统菜单

--- Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/Scripts/Index.js
+++ Sourcecode/Song.WebSite/Templates/OrgAdmin/Default/Scripts/Index.js
@@ -183,7 +183,7 @@ window.$succeeded = function (result) {
             }
         } else throw req.data.message;
     }).catch(err => console.error(err));
-/*
+
     //左上角下拉菜单
     window.drop = window.$dropmenu.create({
         target: '#dropmenu-area',
@@ -200,7 +200,7 @@ window.$succeeded = function (result) {
             console.error(req.data.exception);
             throw req.config.way + ' ' + req.data.message;
         }
-    }).catch(err => console.error(err));*/
+    }).catch(err => console.error(err));
     //竖形工具条
     var vbar = $vbar.create({
         target: '#vbar-area', id: 'rbar-156',
