1d6d1df1e71feee19dc1c0c709a769976d6241b1
weishakeji <10522779@qq.com>
2025-08-02 02:17:19 +0800

    修正细节

--- Document/开发文档/部署说明/start.bat
+++ Document/开发文档/部署说明/start.bat
@@ -1,12 +1,24 @@
-cd iis express
 
+@echo off
+echo 微厦在线学习考试系统 启动中...
+echo.
+
+REM 设置网站路径与端口
+set path=WebSite
+set port=8080
+
+REM 设置网站所在路径，默认为当前当前文件夹
 set BASE_DIR=%~dp0
-set SITE_PATH=%BASE_DIR%WebSite
+set SITE_PATH=%BASE_DIR%%path%
 
-echo %BASE_DIR%
-echo %SITE_PATH%
-start /b iisexpress /path:"%SITE_PATH%" /clr:v4.0 /port:8080 
+::echo %BASE_DIR%
+::echo %SITE_PATH%
 
-start "" "http://localhost:8080/"
+REM 启动web服务器
+cd iis express
+start /b iisexpress /path:"%SITE_PATH%" /port:%port% /clr:v4.0 
+
+REM 打开浏览器，访问网站
+start "" "http://localhost:"%port% 
 
 pause
