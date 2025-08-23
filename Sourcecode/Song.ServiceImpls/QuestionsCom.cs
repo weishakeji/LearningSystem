0070f0630351cd3789e733303bd0fbc2be5d99d2
weishakeji <10522779@qq.com>
2025-08-23 20:15:19 +0800

    优化：增加一些帮助说明

--- Sourcecode/Song.ServiceImpls/QuestionsCom.cs
+++ Sourcecode/Song.ServiceImpls/QuestionsCom.cs
@@ -145,7 +145,8 @@ namespace Song.ServiceImpls
                 if (entity.Ol_ID > 0) wc.And(Questions._.Ol_ID == entity.Ol_ID);
                 if (entity.Cou_ID > 0) wc.And(Questions._.Cou_ID == entity.Cou_ID);
                 //题干是否相同
-                old = Gateway.Default.From<Questions>().Where(wc && Questions._.Qus_Title == entity.Qus_Title && Questions._.Qus_Items == entity.Qus_Items).ToFirst<Questions>();
+                QuerySection<Questions> querySection = Gateway.Default.From<Questions>().Where(wc && Questions._.Qus_Title == entity.Qus_Title && Questions._.Qus_Items == entity.Qus_Items);
+                old = querySection.ToFirst<Questions>();
             }
             if (old == null)
             {
---
+++ Sourcecode/Song.WebSite/Help/ORM/Scripts/index.js
@@ -0,0 +1,26 @@
+﻿$ready(function () {
+    window.vapp = new Vue({
+        el: '#vapp',
+        data: {
+            activeName: 'detail',
+            //菜单项
+            menus: [
+                { name: '简述', tag: 'detail', icon: 'e667', size: 'medium'  },
+                { name: '实体生成', tag: 'entity', icon: 'e85a', size: 'medium'  },   
+             
+            ]
+        },
+        watch: {
+
+        },
+        computed: {
+
+        },
+        mounted: function () {
+
+        },
+        methods: {
+
+        },
+    });
+});
