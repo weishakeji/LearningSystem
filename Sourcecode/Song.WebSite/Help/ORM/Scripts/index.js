14fb57d5315282eaaa4ef6b0e63b748f4ab46d1c
weishakeji <10522779@qq.com>
2025-08-26 11:41:46 +0800

    修正细节

--- Sourcecode/Song.WebSite/Help/ORM/Scripts/index.js
+++ Sourcecode/Song.WebSite/Help/ORM/Scripts/index.js
@@ -5,13 +5,13 @@
             activeName: 'detail',
             //菜单项
             menus: [
-                { name: '简述', tag: 'detail', icon: 'e667', size: 'medium'  },
-                { name: '实体生成', tag: 'entity', icon: 'e85a', size: 'medium'  },   
-                { name: '查询', tag: 'query', icon: 'a00b', size: 'medium'  },   
-                { name: '删改', tag: 'delete', icon: 'e800', size: 'medium'  },  
-                { name: '统计', tag: 'statistic', icon: 'e83c', size: 'medium'  },   
-                { name: '事务', tag: 'trans', icon: 'e634', size: 'medium'  },   
-                { name: '复杂查询', tag: 'complexquery', icon: 'a04d', size: 'medium'  },   
+                { name: '简述', tag: 'detail', icon: 'e667', size: 'medium', completed: true },
+                { name: '实体生成', tag: 'entity', icon: 'e85a', size: 'medium', completed: true },
+                { name: '查询', tag: 'query', icon: 'a00b', size: 'medium' },
+                { name: '删改', tag: 'delete', icon: 'e800', size: 'medium' },
+                { name: '统计', tag: 'statistic', icon: 'e83c', size: 'medium' },
+                { name: '事务', tag: 'trans', icon: 'e634', size: 'medium' },
+                { name: '复杂查询', tag: 'complexquery', icon: 'a04d', size: 'medium' },
             ]
         },
         watch: {
@@ -24,7 +24,12 @@
 
         },
         methods: {
-
+            //获取选项卡的url
+            geturl: function (menu) {
+                if (menu.completed)
+                    return menu.tag + '.htm';
+                else return this.menus[0].tag + '.htm';
+            }
         },
     });
 });
